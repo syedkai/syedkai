c84335e0f7a8dfbf0c0c37ca39339b990fb50586
sweta13 <sweta.swain@stud.fra-uas.de>
2020-11-16 19:23:36 +0100

    Testing File

---
+++ Source/Test1/Test1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "SWETA";
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Let the tasks begin");
+            Console.WriteLine("Task 1 by" + name);
+        }
+    }
+}
