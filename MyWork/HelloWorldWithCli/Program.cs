69202b2d32792bb5f6d6f86ac1bc300897b402b6
Sabin Bajracharya <sabin.bajracharya@gmail.com>
2020-11-19 13:50:36 +0000

    Add: Copyright information to MyWork Project

--- MyWork/HelloWorldWithCli/Program.cs
+++ MyWork/HelloWorldWithCli/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApplication
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World From Console!");
-   
+            Console.WriteLine("@Copyright Sabin Bajracharya 2021");
         }
     }
 }
