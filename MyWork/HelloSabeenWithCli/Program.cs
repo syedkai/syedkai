2dbdf538b62cd983453568263a301c454e76ad95
isabeen <khannssabeen@gmail.com>
2020-11-20 00:05:52 +0500

    Created first applications with Cli and VS

---
+++ MyWork/HelloSabeenWithCli/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloSabeenWithCli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = "Sabeen";
+            String platform = "Cli";
+            Console.WriteLine("Hello " + name + "!");
+            Console.WriteLine($"Congrats on creating your first .Net application with {platform}");
+            Console.WriteLine("Bye!");
+        }
+    }
+}
---
+++ MyWork/HelloSabeenWithVS/HelloSabeenWithVS/HelloSabeenWithVS/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloSabeenWithVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int radius = 34;
+            Console.WriteLine(Math.PI * radius);
+            Console.WriteLine("Sabeen's first application with Visual Studio");
+        }
+    }
+}
