e0ccb57f90d01eeb8ae6d548d6c6c7728d570480
Sabin Bajracharya <sabin.bajracharya@gmail.com>
2020-11-20 22:52:31 +0100

    Add: HelloWorldWithVS solution

---
+++ MyWork/HelloWorldWithVS/ConsoleAppFirst/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to My First Console App!");
+        }
+    }
+}
