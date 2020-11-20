d9d19c346f5ffb719719f3fe7f445332b02d075c
Sabin Bajracharya <sabin.bajracharya@gmail.com>
2020-11-21 00:20:01 +0100

    Change: Make the User class public  - So that it can be access from the application project

--- MyWork/HelloWorldWithClass/UserLibrary/model/User.cs
+++ MyWork/HelloWorldWithClass/UserLibrary/model/User.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleAppWithClass
 {
-    internal class User
+    public class User
     {
         public string Name { get; set; }
         public int Age { get; set; }
