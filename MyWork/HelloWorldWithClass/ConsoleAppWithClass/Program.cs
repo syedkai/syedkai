96c3212f8500f993cc273beb90586f759e434fa6
Sabin Bajracharya <sabin.bajracharya@gmail.com>
2020-11-20 23:45:59 +0100

    Add: User model  - Get input from user and set it to User model and show it

--- MyWork/HelloWorldWithClass/ConsoleAppWithClass/Program.cs
+++ MyWork/HelloWorldWithClass/ConsoleAppWithClass/Program.cs
@@ -6,7 +6,43 @@ namespace ConsoleAppWithClass
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Willkommen zu der Wishing well!");
+            
+            System.Threading.Thread.Sleep(500);
+
+            Console.Write("Wie heißen sie? : ");
+            String name = Console.ReadLine();
+
+            System.Threading.Thread.Sleep(300);
+
+            
+            int age = -1;
+            bool success = false;
+            while (!success)
+            {
+                Console.Write("Wie alt sind Sie? : ");
+                String strAge = Console.ReadLine();
+                success = int.TryParse(strAge, out age);
+            }
+
+            User user = new User
+            {
+               Name = name,
+               Age = age
+            };
+
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Ein Momment bitte!");
+            Console.WriteLine("----------------------------");
+
+            System.Threading.Thread.Sleep(600);
+
+            Console.WriteLine($"Hallo {user.Name}!");
+
+            System.Threading.Thread.Sleep(300);
+
+            Console.WriteLine($"Sie sind {user.Age} Jahre alt.");
+
         }
     }
 }
---
+++ MyWork/HelloWorldWithClass/ConsoleAppWithClass/model/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppWithClass
+{
+    class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public User() { }
+
+        public User(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
