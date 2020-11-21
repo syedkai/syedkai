1087d0fb9eebca3cfd850cb99adaa2eb7b33b106
junaid Khalid <junaidkhalid185@gmail.com>
2020-11-22 01:58:52 +0500

    adding complete 1st prog files

---
+++ Source/My1stProg/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace My1stProg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=0;i<=4;i++)
+            {
+            Console.WriteLine("Hello World!");
+            
+
+             }
+        }
+    }
+}
