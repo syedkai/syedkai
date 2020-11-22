2c2f1ce926ea1cbfebada94a11deb75538d5776e
sweta13 <sweta.swain@stud.fra-uas.de>
2020-11-22 15:07:31 +0100

    My first C# code and commit

---
+++ MyWork/HelloWorldWithCli/HelloWorldWithCli/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorldWithCli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I created my first project");
+            Console.Write("Enter your fav colour: ");
+            string fav_color = Console.ReadLine();
+            Console.Write("YOUR FAV COLOUR IS " + fav_color);
+            Console.ReadLine();
+        }
+    }
+}
