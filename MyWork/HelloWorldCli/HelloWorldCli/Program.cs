1e34b1d6f4d7abb9be68a5427c5719dbb4e4ddfc
Usman Ali <ausman882@gmail.com>
2020-11-22 14:51:51 +0100

    My first commit

---
+++ MyWork/HelloWorldCli/HelloWorldCli/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorldCli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("My SE Project"); 
+        }
+    }
+}
