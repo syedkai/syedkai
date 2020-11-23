68d5a10c9ecbf9aefec56b3e3c44bd52ee7dea9e
Saloni <salgoyal1503@gmail.com>
2020-11-23 16:59:34 +0530

    project created in VS

---
+++ MyWork/VSProj1/HelloVS/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+           var a= Console.ReadLine();
+            Console.WriteLine("Entered text is :");
+            Console.WriteLine(a);
+        }
+    }
+}
