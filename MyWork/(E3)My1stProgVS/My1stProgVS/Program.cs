e73087fcd83da422769cccd02279979e5f37f0b0
junaid Khalid <junaidkhalid185@gmail.com>
2020-11-25 14:43:45 +0500

    E1 is done

---
+++ MyWork/(E3)My1stProgVS/My1stProgVS/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace My1stProgVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Please enter the radius = ");
+            String radius = Console.ReadLine();                            // Input given by user and stored in radius
+            double area = Math.Pow(double.Parse(radius), 2) * Math.PI;    // String converted into double and power ( A, Power)
+
+            Console.WriteLine($"Area of the Circle is {area}");
+        }
+
+
+
+    }
+}
---
+++ Source/ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        public static void Main(string[] args)
+        {
+            int a = 22;
+            double b = 39.01;
+            decimal d = 40.123M;
+
+
+            double ANS = (double)a * (double)b / (double)d;
+
+
+            Console.WriteLine($"Answer  = {ANS}");
+
+        }
+    }
+}
