85d725b24f597f1c0a8d66e2d60b53fbc3088cea
junaid Khalid <junaidkhalid185@gmail.com>
2020-11-25 20:41:35 +0500

    Lesson 3 Exercise done

---
+++ MyWork/Program3/ClassLibrary1/ReadMulti.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program3
+{
+     public class ReadMulti
+    {
+        public static int Read(string a)
+        {
+            int b = a.Length;
+            return b;
+
+        }
+
+
+        public static int Multi (int a, int b)
+        {
+
+            return (a * b);
+
+        }
+    }
+}
---
+++ MyWork/Program3/Program3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Program3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number ");                           // Program to Multiply two integers
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a 2nd number");
+            int b = int.Parse(Console.ReadLine());
+
+            int result = ReadMulti.Multi(a,b);
+
+            Console.WriteLine($" Multiplication result is {result}");
+
+            Console.WriteLine("Enter a String to get its length");           // Program to calculate the lenght of ta string
+            string c = Console.ReadLine();
+            int len = ReadMulti.Read(c);
+
+            
+            Console.WriteLine($" length of // {c} // is {len}");
+        }
+    }
+}
