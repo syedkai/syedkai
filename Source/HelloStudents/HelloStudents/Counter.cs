774e5f9deaafdf8463cd3eb9b17d1359566b56aa
chiemerie-arum <emerie.arum@yahoo.com>
2020-11-27 23:49:44 +0100

    initial commit, added some files

---
+++ Source/HelloStudents/HelloStudents/Counter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HelloStudents
+{
+    class Counter
+    {
+        public void IncrementalCounter()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(250);
+            }
+        }
+
+        public void DecrementalCounter()
+        {
+            for (int i = 50; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(250);
+            }
+        }
+    }
+}
--- Source/HelloStudents/HelloStudents/Program.cs
+++ Source/HelloStudents/HelloStudents/Program.cs
@@ -7,23 +7,42 @@ namespace HelloStudents
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BetterCalculator bCalc = new BetterCalculator();
 
-           /* MyAlgorithm alg = new MyAlgorithm();
+            double res = bCalc.Add(221, 42343);
+            double res2 = bCalc.Mul(221, 42343);
+            double res3 = bCalc.Div(221, 42343);
 
-            for (int i = 0; i < 10; i++)
-            {
-                double result = alg.Calculate(i);
+            Console.WriteLine(res);
+            Console.WriteLine();
+            Console.WriteLine(res2);
+            Console.WriteLine();
+            Console.WriteLine(res3);
 
-                Console.WriteLine("Haritha Test \n changes");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
 
-                Console.WriteLine($"Hello Students from Windows! Iteration: {i} generated the result = {result}");
+            Counter count = new Counter();
+            count.IncrementalCounter();
+            Console.WriteLine();
+            Thread.Sleep(2000);
+            count.DecrementalCounter();
 
-                Thread.Sleep(500);
-            }
+            /* MyAlgorithm alg = new MyAlgorithm();
 
-            Console.WriteLine("Press any key do exit...");
-            Console.ReadLine();*/
+             for (int i = 0; i < 10; i++)
+             {
+                 double result = alg.Calculate(i);
+
+                 Console.WriteLine("Haritha Test \n changes");
+
+                 Console.WriteLine($"Hello Students from Windows! Iteration: {i} generated the result = {result}");
+
+                 Thread.Sleep(500);
+             }
+
+             Console.WriteLine("Press any key do exit...");
+             Console.ReadLine();*/
         }
     }
 }
