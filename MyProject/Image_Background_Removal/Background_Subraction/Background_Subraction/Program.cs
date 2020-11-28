ed6926d5e081081dd47ef27972be4d5451e1f095
usman62 <usmaan.awaan@gmail.com>
2020-11-28 18:42:13 +0100

    Project

---
+++ MyProject/Image_Background_Removal/Background_Subraction/Background_Subraction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Background_Subraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
