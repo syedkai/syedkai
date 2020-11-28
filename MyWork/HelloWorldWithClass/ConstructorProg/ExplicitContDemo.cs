74c6be654beba294f6e1750db1aff51edf1cc7c3
SHIVAM KAUSHIK <shivamkaushik007@yahoo.com>
2020-11-28 18:02:33 +0530

    Program Created with different classes

---
+++ MyWork/HelloWorldWithClass/ConstructorProg/ExplicitContDemo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorProg
+{
+    class ExplicitContDemo
+    {
+        public ExplicitContDemo()
+        {
+            Console.WriteLine("Constructor is called");
+        }
+
+        static void Main()
+        {
+
+            ExplicitContDemo obj1 = new ExplicitContDemo();
+
+            
+        }
+
+    }
+}
---
+++ MyWork/HelloWorldWithClass/ConstructorProg/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorProg
+{
+    class Program
+    {
+        int i;
+        static void Main(string[] args)
+        {
+            Program p = new Program ();
+
+            Console.WriteLine("The value of i is: " +p.i);
+
+            
+        }
+    }
+}
