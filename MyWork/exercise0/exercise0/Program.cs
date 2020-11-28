64b457dca63fce113a15a655efc05cc5de97b4b8
oluwatobi Adeenkan <tadenekan@dlabs.cloud>
2020-11-28 22:41:08 +0100

    chore: Changed exercise zero to one.

--- MyWork/exercise0/exercise0/Program.cs
+++ MyWork/exercise0/exercise0/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 
-namespace exercise0
+namespace Exercise0
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello my first c# ");
+            Console.WriteLine("I hope i really love this");
+            Console.WriteLine("All the best to me");
         }
     }
 }
