bc0446c03bb0778bc2f1603182c37f82dbf83f8a
Kanav <apple@Kanavs-MacBook-Air.local>
2020-11-28 21:24:24 +0100

    New Project

---
+++ MyWork/HelloWorldWithCli/HelloWorldWithCli/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HelloWorldWithCli
+{
+    class public Program
+    {
+        static public void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is my first project with C#");
+            Console.WriteLine("Addition of two Integers");
+            Console.WriteLine("Enter First Integer:");
+            string line1 = Console.ReadLine();
+            int a = int.TryParse(line1);
+            Console.WriteLine("Enter Second Integer:");
+            string line2 = Console.ReadLine();
+            int b = int.TryParse(line2);
+            int c = a + b;
+            Console.WriteLine("The sum is: " + c);
+
+
+         
+
+
+        }
+    }
+}
