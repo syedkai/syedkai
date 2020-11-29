0a4db5b7b5e343141f0704187b1d4e04f91930ce
SnigdhaKantam <snigdhakatm@gmail.com>
2020-11-30 02:10:24 +0530

    Library project referenced in main project Example

---
+++ MyWork/HelloWorldWithVS/CalucatingProject/Calculator/Operations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Operations
+    {
+        public static int Multiply(int a,int b)
+        {   
+            return a*b;
+        }
+        public int Add(int a, int b)
+        {
+            return a+b;
+        }
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public static double Divide(int a, int b)
+        {
+            return (double)a/b;
+        }
+    }
+}
---
+++ MyWork/HelloWorldWithVS/CalucatingProject/CalucatingProject/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Calculator;
+
+namespace CalucatingProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to calculator.Please enter two numbers and operation symbol");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            string symbol = Console.ReadLine();
+            Operations calObj = new Operations();
+            var result = 0;
+            switch (symbol)
+            {
+                case "+":
+                    result = calObj.Add(a, b);
+                    Console.WriteLine(result);
+                    break;
+                case "-":
+                    result = calObj.Subtract(a, b);
+                    Console.WriteLine(result);
+                    break;
+                case "*":
+                    result = Operations.Multiply(a, b);
+                    Console.WriteLine(result);
+                    break;
+                case "/":
+                    double res = Operations.Divide(a, b);
+                    Console.WriteLine(res);
+                    break;
+                default:
+                    Console.WriteLine("Please enter symbols only +,*,/,-");
+                    break;
+            }
+        }
+    }
+}
