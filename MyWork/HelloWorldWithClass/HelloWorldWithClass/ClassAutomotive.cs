6dbbcb3adde8dab585c3806ebf8d84d8f3334480
sweta13 <sweta.swain@stud.fra-uas.de>
2020-11-29 20:01:38 +0100

    small program on class

---
+++ MyWork/HelloWorldWithClass/HelloWorldWithClass/ClassAutomotive.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorldWithClass
+{
+    public class ClassAutomotive
+    {
+        string carBrand;
+        string carColour;
+        int carManfactureYear;
+
+        public ClassAutomotive( string brand, string colour, int yearOfManufacture)
+        {
+            this.carBrand = brand;
+            this.carColour = colour;
+            this.carManfactureYear = yearOfManufacture;
+        }
+
+        public string getCarBrand()
+        {
+            return carBrand;
+        }
+
+        public string getCarColour()
+        {
+            return carColour;
+        }
+
+        public int getYearOfManufacture()
+        {
+            return carManfactureYear;
+        }
+
+        public string getTheCarDetails()
+        {
+            return ("This is a " + this.getCarBrand() + " in colour " + this.getCarColour() + " and it was manufactured in the year " + this.getYearOfManufacture() );
+        }
+
+
+    }
+}
---
+++ MyWork/HelloWorldWithClass/HelloWorldWithClass/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorldWithClass
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ClassAutomotive Car = new ClassAutomotive("BMW", "Blue", 1991);
+            Console.WriteLine(Car.getTheCarDetails());
+        }
+    }
+}
