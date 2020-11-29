f1d03331ee79778ec86fce740c1f71062e6155a8
Kanav <apple@Kanavs-MacBook-Air.local>
2020-11-29 23:47:53 +0100

    Experimenting with Classes

---
+++ MyWork/HelloWorldWithClass/HelloWorldWithClass/ClassStudentInformation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+
+namespace HelloWorldWithClass
+{
+    public class ClassStudentInformation  
+    {
+        string Author;
+        string Title;
+        string Publisher;
+
+        public ClassStudentInformation(string a,string t, string p)
+        {
+            this.Author = a;
+            this.Title = t;
+            this.Publisher = p;
+
+        }
+
+        public string getAuthorName()
+        {
+            return Author;
+        }
+
+        public string getTitle()
+        {
+            return Title;
+        }
+
+        public string getPublisher()
+        {
+            return Publisher;
+        }
+
+        public string Output()
+        {
+
+            return ("Title: " + this.getTitle() + " Author: " + this.getAuthorName()+ " Publisher: " + this.getPublisher()); 
+            
+           
+        }
+   
+    }
+}
---
+++ MyWork/HelloWorldWithClass/HelloWorldWithClass/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorldWithClass
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ClassStudentInformation newBook = new ClassStudentInformation("Robert T Kiyosaki", "Rich Dad Poor Dad", "Penguin Publishers");
+            Console.WriteLine(newBook.Output());
+          
+
+        }
+    }
+}
