efcbaea548a6fe05301576ebccacabf65ced5b7b
onyemakn <onyemakizito@outlook.com>
2020-11-29 21:34:04 +0100

    New Project App

---
+++ MyWork/KO App/KO App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KO_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
