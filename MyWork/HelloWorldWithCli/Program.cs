b783b24c8a83d9d17b38c7c21a035501dbb0d7ae
SnigdhaKantam <snigdhakatm@gmail.com>
2020-11-30 00:18:05 +0530

    My custom program

--- MyWork/HelloWorldWithCli/Program.cs
+++ MyWork/HelloWorldWithCli/Program.cs
@@ -6,7 +6,10 @@ namespace HelloWorldWithCli
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is my first application using command line!");
+            Console.WriteLine("Please add a message for me:");
+            var msg = Console.ReadLine();
+            Console.WriteLine("Your message for me is {0}",msg);
         }
     }
 }
