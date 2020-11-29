467fe24a4a014d98697df2653ae65dfe4eab7635
SnigdhaKantam <snigdhakatm@gmail.com>
2020-11-30 01:27:31 +0530

    Committing console app with VS

---
+++ MyWork/HelloWorldWithVS/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First console application with VS!");
+            Console.WriteLine("Please enter numbers to count :");
+            int a = Convert.ToInt32(Console.ReadLine());
+            for(var i=0; i <= a; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
