2b10302da4c07fa045b4f8580a9143ed3d072683
SHIVAM KAUSHIK <shivamkaushik007@yahoo.com>
2020-11-30 00:42:35 +0530

    Console Application created usingb Command Prompt

---
+++ MyWork/HelloworldWithCli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloworldWithCli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
