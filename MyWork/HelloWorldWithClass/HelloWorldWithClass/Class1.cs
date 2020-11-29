e14a55e5d8af3b708dcf92772419fb2d7da0eee6
SHIVAM KAUSHIK <shivamkaushik007@yahoo.com>
2020-11-29 17:40:12 +0530

    Program created with 2 classes

---
+++ MyWork/HelloWorldWithClass/HelloWorldWithClass/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldWithClass
+{
+    class Class1
+    {
+       
+
+        public Class1 ()
+        {
+            Console.WriteLine("Constructor is called");
+
+        }
+        static void Main()
+
+        {
+
+            Class1 obj = new Class1();
+
+            
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ MyWork/HelloWorldWithClass/HelloWorldWithClass/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldWithClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Hello World");
+
+            Console.WriteLine("Hello World");
+
+
+
+        }
+    }
+
+}
