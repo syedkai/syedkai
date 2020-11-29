b00aedd97127fb9370404666f84ef0615eca1ab2
SHIVAM KAUSHIK <shivamkaushik007@yahoo.com>
2020-11-29 23:21:57 +0530

    ClassLibrary2 is created and it is used in a program called ClassLibraryCreated_UsedInConsoleApplication

---
+++ MyWork/ClassLibrary2/ClassLibrary2/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassLibrary2
+{
+    public class Class1
+    {
+        int i;
+
+        public int sum(int a, int b)
+
+        {
+            i = a + b;
+
+            return i;
+        
+        }
+
+        
+
+
+
+
+    }
+
+
+
+
+
+}
---
+++ MyWork/ClassLibraryCreated_UsedInConsoleApplication/ClassLibraryCreated_UsedInConsoleApplication/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary2;
+
+namespace ClassLibraryCreated_UsedInConsoleApplication
+{
+
+    
+    class Program
+    {
+
+       
+        static void Main(string[] args)
+        {
+
+            Class1 obj = new Class1();
+
+
+             Console.WriteLine("The final result of addition is " +obj.sum(30, 35));
+
+            Console.ReadLine();
+
+        }
+    }
+}
