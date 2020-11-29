833f80d1e6c0eea3f4812574d8779c0f9e519579
khanparizat <khan.rahman@stud.fra-uas.de>
2020-11-29 09:40:45 -0600

    Commiting first C# code with vs

---
+++ MyWork/test1/test1/test1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello everyone");
+            Console.WriteLine("");
+            Console.WriteLine("just playing with VS");
+        }
+    }
+}
