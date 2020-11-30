e76916876c9be17c36a3949f5d740446b27f6e11
Haritha G <harytha6@gmail.com>
2020-11-30 13:53:41 +0100

    Rearranging files

---
+++ MyWork/HelloWorldWithCli - E2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// String Array , For Loop testing
+namespace myApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            var namens = new List<string> { "Haritha", "Freya", "x", "y" };
+            foreach (var namen in namens)
+            {
+                Console.WriteLine($"Hello {namen.ToUpper()} ");
+            }
+        }
+    }
+}
