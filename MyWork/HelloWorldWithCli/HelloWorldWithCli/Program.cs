6a4d2809c9f13313790ce838bc2f4933e6ba4876
Bilal Shaikh <26bilal@gmail.com>
2020-11-30 17:50:18 +0100

    My first Commit

---
+++ MyWork/HelloWorldWithCli/HelloWorldWithCli/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorldWithCli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(" My first commit"); 
+
+        }
+    }
+}
