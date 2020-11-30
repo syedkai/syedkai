719b1eb8e0c78b706525930f320aa325d8d6506c
Shivakumar <shivakumar.haralahallimariyappa@stud.fra-uas.de>
2020-11-30 23:19:48 +0530

    New commit

---
+++ MyWork/HelloWorldWith_CLI/HelloWorldWithCli/HelloWorldWithCli/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorldWithCli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my first application using command line!");
+            Console.WriteLine("Please add a message for me:");
+            Console.WriteLine("Please add a message for me:");
+            var msg = Console.ReadLine();
+            Console.WriteLine("Your message for me is {0}", msg);
+        }
+    }
+}
