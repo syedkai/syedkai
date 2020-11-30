ccb9280b762fcc28445e1a9af04373bf5446eaf3
Mehadi <hmd.mehadi@yahoo.com>
2020-11-30 22:14:24 +0100

    first program with Visual Studio...E3

---
+++ MyWork/ProjectWithVS/HelloWorldWithVS/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace HelloWorldWithVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            for(int i=0;i<10;i++)
+            {
+            Console.WriteLine($"Hello World!: {i}");
+            Thread.Sleep(500);
+                
+            }
+        }
+    }
+}
