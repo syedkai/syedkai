0d9765f42a39c77928d9e42181bcae4de6d4944c
Shivakumar <shivakumar.haralahallimariyappa@stud.fra-uas.de>
2020-12-01 00:40:50 +0530

    New commit

---
+++ MyWork/HelloWorldWithCli/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorldWithCli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Welcome to my CLI program");
+            Console.WriteLine("My Basic console print and for operation");
+			for(int i = 0; i < 10; i++)
+            {
+                 Console.WriteLine("Value of i: {0}", i);
+            }
+        }
+    }
+}
---
+++ MyWork/HelloWorldWithVS/Square/Square/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a square program. Please enter a number till which you want squares:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            for (int i = i; i < 10; i++)
+            {
+                Console.WriteLine($"Iteration: {i} generated the result = {i * i}");
+
+            }
+        }
+    }
+}
