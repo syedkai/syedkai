d14997520222f9dbd983c0ca2ac5d5e77977d31d
Usman Ali <ausman882@gmail.com>
2020-11-30 10:43:17 +0100

    four commit

---
+++ MyWork/Alisoftware/HelloWorldWithCLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldWithCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
