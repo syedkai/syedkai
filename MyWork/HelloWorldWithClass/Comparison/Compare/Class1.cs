59e8950a92b2466a3cbaed9fb951b6d3eff9c0e2
Shivakumar <shivakumar.haralahallimariyappa@stud.fra-uas.de>
2020-12-01 01:17:50 +0530

    E1 to E4

---
+++ MyWork/HelloWorldWithClass/Comparison/Compare/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Compare
+{
+    public class CompareClass
+    {
+        public static int Comparer(int a, int b, string letter)
+        {
+            if (letter == "g")
+            {
+                return a > b ? a : b;
+            }
+            else
+            {
+                return a < b ? a : b;
+            }
+        }
+    }
+}
---
+++ MyWork/HelloWorldWithClass/Comparison/Comparison/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Compare;
+
+namespace Comparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is comparison program. Please enter two numbers");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter letter g for greatest number and l for least number");
+            string input = Console.ReadLine();
+            if(input=="g" || input == "l")
+            {
+                int c = CompareClass.Comparer(a, b, input);
+                Console.WriteLine("Result:{0}", c);
+            }
+            else
+            {
+                Console.WriteLine("You have not entered correct letter");
+            }
+        }
+    }
+}
