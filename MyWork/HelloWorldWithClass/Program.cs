9f7eb39a1be8a58ac7afc13772c89344d3a2051a
chiemerie-arum <emerie.arum@yahoo.com>
2020-11-30 18:45:46 +0100

    added a new project to the solution

---
+++ MyWork/HelloWorldWithClass/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorldWithClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            Console.WriteLine("Welcome, User! Please Enter your name");
+            string name = user.getName();
+            Console.WriteLine($"Hi {name}! How old are you?");
+            int age = user.getAge();
+            Console.WriteLine($"Welcome {name}! You are {age} years old :)");
+        }
+    }
+}
---
+++ MyWork/HelloWorldWithClass/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldWithClass
+{
+    class User
+    {
+        private string Name;
+        private int Age;
+
+        public User()
+        {
+            Name = " ";
+            Age = 0;
+        }
+
+        public string getName()
+        {
+            Name = Console.ReadLine();
+            return Name;
+        }
+
+        public int getAge()
+        {
+            Age = int.Parse(Console.ReadLine());
+            return Age;
+        }
+    }
+}
