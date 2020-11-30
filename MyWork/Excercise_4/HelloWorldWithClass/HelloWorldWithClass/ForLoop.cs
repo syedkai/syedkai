d0e3da4b49142fe7c13308e2aad62cdd8e7c6218
Joseph Itopa <grabjosphab@gmail.com>
2020-11-30 11:58:10 +0100

    Solution to excercises, 1 through 4

---
+++ MyWork/Excercise_4/HelloWorldWithClass/HelloWorldWithClass/ForLoop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorldWithClass
+{
+    class ForLoop
+    {
+        static void Main(string[] args)//main method
+        {
+
+            public int ntime(int a)
+
+            {
+                int a = 0; //Instantiate a variable
+
+                Console.WriteLine("Enter the number of times HelloWorld should be displayed");
+
+                a = int.Parse(Console.ReadLine()); // input value
+
+                return a;
+
+            }
+
+        }
+    }
+}
