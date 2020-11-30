71829ce6fc04fed4bd4dec826fd58a73070ec347
Abbad Zafar <abbad.zafar@stud.fra-uas.de>
2020-11-30 12:56:33 +0500

    Excersice 4 Program with Class

---
+++ MyWork/Exercise4/AdditionClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise4
+{
+    class AdditionClass
+    {
+        public int Add(int a, int b) //funcion of class
+        {
+            return a + b; //substraction of the class
+        }
+    }
+}
---
+++ MyWork/Exercise4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int var1 = 2;
+            int var2 = 5;
+            int Result; 
+            
+
+            AdditionClass add = new AdditionClass(); 
+            Result = add.Add(var1, var2); 
+
+            Console.WriteLine("Result:" + Result);
+           
+        }
+    }
+}
