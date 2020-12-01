c5a4fe80d01f18a9d21d9d936fad0bb2ddba6ffc
Haritha G <harytha6@gmail.com>
2020-12-01 15:08:06 +0100

    Debug, Breakpoints, Immediate window

---
+++ MyWork/Haritha/Additional Project - E4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+// testing breakpoints and tracking value of variables in immediate window 
+namespace Additional_Project___E4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int a = 10;
+            int b = 15;
+
+            Console.WriteLine("Values: {0} {1} ", a, b);
+        }
+    }
+}
