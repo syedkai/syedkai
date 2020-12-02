63b016c45371d47f8b2f18ef6d95200fc2673cf1
Haritha G <harytha6@gmail.com>
2020-12-02 15:12:48 +0100

    TaskCompletion Status

---
+++ MyWork/Haritha/AsyncTaskCompletionStatus/AsyncTaskCompletionStatus/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncTaskCompletionStatus
+{
+    class Program
+    {
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Task Completion Status");
+            var t1 = Task.Delay(10000);
+            Console.WriteLine("Task1 initialized");
+            Console.WriteLine("Task1 completed true or false?");
+            Console.WriteLine(t1.IsCompleted);
+            // t1 not complete , but t2 executes 
+            // without await, delay happens separately
+            Console.WriteLine("Task2 initialized");
+            var t2 = Task.Delay(10000);
+            await t2;   // await makes t2 finish and then only t3 starts
+                        // 10 seconds delay 
+            Console.WriteLine("Task2 completed true or false?");
+            Console.WriteLine(t2.IsCompleted);
+            Console.WriteLine("Task 3");
+        
+        
+        }
+    }
+}
