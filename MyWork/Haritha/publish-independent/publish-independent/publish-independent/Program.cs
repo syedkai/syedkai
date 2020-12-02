ccebaf6758e5ff7ec145d25fd75b1e9e19ece7a5
Haritha G <harytha6@gmail.com>
2020-12-02 14:14:08 +0100

    Publishing builds and varying RunTimeIdentifiers Changing Target Frameworks

---
+++ MyWork/Haritha/publish-independent/publish-independent/publish-independent/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+// changed to net5.0 standard
+// published file in windows 64 and 32 bit architecture and 
+// run the program in command prompt
+// created build version for MAC OS
+
+namespace publish_independent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Changed dotnet Standard and RunTimeIdentifiers");
+        }
+    }
+}
