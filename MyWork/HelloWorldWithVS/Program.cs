c81593a96588cd4af79ea5c6414d30697a340e8a
Janet007 <root@Janets-MacBook-Pro.local>
2020-12-02 01:15:12 +0100

    HelloWorldWithVSadded

---
+++ MyWork/HelloWorldWithVS/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorldWithVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I am Janet");
+            Console.Write("Happy");
+            Console.Write("December:)");
+            Console.ReadLine();
+
+        }
+    }
+}
