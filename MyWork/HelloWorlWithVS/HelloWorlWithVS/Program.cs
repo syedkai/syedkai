d22c323e3bf72cf7315822fe45481da9d2e0ac49
Kanav <apple@Kanavs-MacBook-Air.local>
2020-12-02 17:44:09 +0100

    Testing

---
+++ MyWork/HelloWorlWithVS/HelloWorlWithVS/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HelloWorldWithVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            ///checking inbuilt sort function
+
+            int[] arr1 = { 55, 34, 23, 57, 89, 65, 76 };
+
+            var numlist = new List<int>();
+            numlist.AddRange(arr1);
+
+            numlist.Sort();
+            foreach (var arr in numlist)
+                Console.WriteLine(arr);
+
+            Console.WriteLine();
+        }
+    }
+}
+
