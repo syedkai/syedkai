faa243f40c043aea7f82351a35556ec734e3e23b
Sabin Bajracharya <sabin.bajracharya@gmail.com>
2020-12-03 20:11:04 +0100

    Add: Demo project to showcase Configuration Usuage

---
+++ MyWork/Configuration/Configuration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Configuration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Project to demonstrate configuration usuage");
+        }
+    }
+}
