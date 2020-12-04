be56a7f6b11d90ec8ceb588d657b3f4022442e66
newaz96 <74206051+newaz96@users.noreply.github.com>
2020-12-04 11:24:45 +0600

    Changes on text

---
+++ MyWork/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Newazapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=1; i<200; i++)
+                Console.WriteLine($"Hello viewers from Windows10! Iteration= {i}");
+                System.Threading.Thread.Sleep(100);
+        }
+    }
+}
