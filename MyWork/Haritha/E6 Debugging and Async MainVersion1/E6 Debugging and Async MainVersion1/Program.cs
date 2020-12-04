655097e588be6e24441dfcc6088cc93385dad41c
Haritha G <harytha6@gmail.com>
2020-12-04 09:55:37 +0100

    E6 - debug & Async mainv1

---
+++ MyWork/Haritha/E6 Debugging and Async MainVersion1/E6 Debugging and Async MainVersion1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace E6_Debugging_and_Async_MainVersion1
+{
+    // async task main - return type is task
+    // await task.delay()
+
+    //breakpoint attached during first iteration of for loop
+    // i value read in immediate window
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello! testing Async Main in Counter");
+            int n = 50;
+            for (int i =0; i < n; i++)
+            {
+                Console.WriteLine($"The count right now is {i}");
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
