d8cd0a26bfb535295d8abd97a4bfe089f937bf6b
newaz96 <74206051+newaz96@users.noreply.github.com>
2020-12-05 20:49:07 +0600

    Exercise E3

---
+++ MyWork/Exercise E3/HelloWorldWithVS/HelloWorldWithVS/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            for(int i=1; i<50; i++)
+            Console.WriteLine("Welcome viewers from windows ):");
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+        }
+    }
+}
+
