45f0a22b3f08e3ff0cab59b5d3679922487fb87e
Md.Mukit Khan <mukitkhan07@gmail.com>
2020-12-05 02:35:07 -0800

    1.StartEachJobInOwnThread 2. StartJobsInParallelTask 3. Dynamic core count and version detection

---
+++ MyWork/Ex9_Mukit/ThreadingSampleNetCorev5.0/ThreadingSampleNetCorev5.0/ParallelSamples.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadingSampleNetCorev5._0
+{
+    class ParallelSamples
+     {
+
+        private long m_FinishCounter = 0;
+
+        public void StartJobInSeqence(int sequences, Action<object> func)
+        {
+
+            Thread.CurrentThread.Name = "Single Main Thread";
+
+            for (int i = 0; i < sequences; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Job Sequence No: {0}", i);
+                func(null);
+            }
+
+
+
+        }
+
+
+       
+
+        public void StartEachJobInOwnThread(int threads, Action<object> func)
+        {
+             for (int i = 0; i < threads; i++)
+            {
+                var t = new Thread(new ParameterizedThreadStart(func));
+                t.Name = i.ToString();
+                t.Start(new Action<string>(OnThreadFinished));
+            }
+            while (true)
+            {
+                if (Interlocked.Read(ref m_FinishCounter) == threads)
+                    break;
+                else
+                    Thread.Sleep(500);
+            }
+        }
+
+
+        private void OnThreadFinished(string threadName)
+        {
+            Interlocked.Increment(ref m_FinishCounter);
+        }
+
+
+
+        public void StartJobsInParallelTask(int threads, Action<object> func) {
+
+
+            List<Task> tList = new List<Task>();
+
+            for (int i = 0; i < threads; i++)
+            {
+                var t = new Task(func, new Action<string>(OnThreadFinished));
+                tList.Add(t);
+            }
+
+            foreach (var t in tList)
+            {
+                t.Start();
+            }
+           
+            Task.WaitAll(tList.ToArray());
+
+        }
+
+    }
+}
---
+++ MyWork/Ex9_Mukit/ThreadingSampleNetCorev5.0/ThreadingSampleNetCorev5.0/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ThreadingSampleNetCorev5._0
+{
+    class Program
+    {
+
+       
+        
+        static int numThreads= 6, coreCount = 0;
+        static Stopwatch sw;
+        static String runtimenetCoreVer;
+
+        static void Main(string[] args)
+        {
+            
+
+
+
+            Console.WriteLine("Press any Key to start.");
+            Console.ReadKey();
+           // var netCoreVer = System.Environment.Version;
+            runtimenetCoreVer  = System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription;
+            Console.WriteLine(runtimenetCoreVer);
+            SystemCoreCount();
+            sw = new Stopwatch();
+            sw.Start();
+
+
+
+            ParallelSamples sample = new ParallelSamples();
+            sample.StartJobInSeqence(numThreads, Workerfuntion);
+            sw.Stop();
+            FormatOutput(1,1);
+
+       
+            sw.Restart();
+            sample.StartEachJobInOwnThread(numThreads, Workerfuntion);
+            sw.Stop();
+            FormatOutput(2, numThreads);
+
+            sw.Restart();
+            sample.StartJobsInParallelTask(numThreads, Workerfuntion);
+            sw.Stop();
+            FormatOutput(3, numThreads);
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+        }
+
+
+
+
+
+        private static void Workerfuntion(Object onFinishDelegate)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Started thread: {Thread.CurrentThread.Name} - {Thread.CurrentThread.ManagedThreadId}");
+            int r = 1;
+
+            for (int i = 0; i < 35000899; i++)
+            {
+                r = r + 1;
+            }
+
+            if (onFinishDelegate != null)
+            {
+                ((Action<string>)onFinishDelegate)(Thread.CurrentThread.Name);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Stopped thread: {0}", Thread.CurrentThread.Name);
+
+        }
+
+
+        private static void FormatOutput(int p_type,int numberOfthreds)
+        {
+            switch (p_type)
+            {
+                case 1:
+                    Console.WriteLine( "Job Approach: Sequencial; framework_version: "+ runtimenetCoreVer+ " cores: " + coreCount + " total_threads: "+ numberOfthreds + " elasped time: " + " h{0} ms", sw.ElapsedMilliseconds);
+                    break;
+                case 2:
+                    Console.WriteLine("Job Approach: Job in Each Thread; framework_version: " + runtimenetCoreVer + " cores: " + coreCount + " total_threads: " + numberOfthreds + " elasped time: " + " h{0} ms", sw.ElapsedMilliseconds);
+                    break;
+                case 3:
+                    Console.WriteLine("Job Approach: Parallel Task; framework_version: " + runtimenetCoreVer + " cores: " + coreCount + " total_threads: " + numberOfthreds + " elasped time: " + " h{0} ms", sw.ElapsedMilliseconds);
+                    break;
+                 
+
+            }
+        }
+
+        private static void SystemCoreCount()
+        {
+            coreCount = Environment.ProcessorCount;
+        }
+
+       
+
+
+
+   
+    }
+}
