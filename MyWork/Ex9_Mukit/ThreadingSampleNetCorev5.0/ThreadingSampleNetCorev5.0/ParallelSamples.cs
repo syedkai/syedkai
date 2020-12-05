0f59d3b58bdf8ac5d0d73cb139bd0d56dbb6de5a
Md.Mukit Khan <mukitkhan07@gmail.com>
2020-12-05 02:35:07 -0800

    Run a job in Sequence 1.1 Execution job after job in .NET Core 5.0

---
+++ MyWork/Ex9_Mukit/ThreadingSampleNetCorev5.0/ThreadingSampleNetCorev5.0/ParallelSamples.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadingSampleNetCorev5._0
+{
+    class ParallelSamples
+    {
+        public void StartJobInSeqence(int sequences , Action<object>func) {
+
+            Thread.CurrentThread.Name = "Single Main Thread";
+
+
+            for (int i = 0; i < sequences; i++) {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Job Sequence No: {0}", i);
+                func(null);
+            }
+
+        }
+
+
+  
+
+        public void StartJobInMulitithred() { 
+        }
+
+        public void StartJobInParallel() { }
+
+    }
+}
---
+++ MyWork/Ex9_Mukit/ThreadingSampleNetCorev5.0/ThreadingSampleNetCorev5.0/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ThreadingSampleNetCorev5._0
+{
+    class Program
+    {
+
+
+        static int numSequence = 100;
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Press any Key to start.");
+            Console.ReadKey();
+           // var netCoreVer = System.Environment.Version;
+            var runtimenetCoreVer = System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription;
+            Console.WriteLine(runtimenetCoreVer);
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            ParallelSamples sample = new ParallelSamples();
+            sample.StartJobInSeqence(numSequence, Workerfuntion);
+            sw.Stop();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(runtimenetCoreVer+ " has taken {0} ms to run the job in Sequence", sw.ElapsedMilliseconds);
+            Console.ReadLine();
+        }
+
+        private static void Workerfuntion(Object onFinishDelegateFunction)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Started thread: {Thread.CurrentThread.Name} - {Thread.CurrentThread.ManagedThreadId}");
+            int r = 1;
+      
+            for (int i = 0; i < 35000899; i++)
+            {
+                r = r + 1; 
+            }
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Stopped thread: {0}", Thread.CurrentThread.Name);
+           
+        }
+
+
+
+   
+    }
+}
