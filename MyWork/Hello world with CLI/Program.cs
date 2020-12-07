20d0cf8a74f44c898064a76790f0ed479cda0a5b
Arup Kumar Gope <gope.aust@gmail.com>
2020-12-07 20:36:41 +0100

    Hello World with CLI

---
+++ MyWork/Hello world with CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello_world_with_CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! with CLI");
+        }
+    }
+}
