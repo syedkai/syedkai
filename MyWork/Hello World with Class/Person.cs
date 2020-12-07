738d7945df6df20c82e0d461c299a0509df12c85
Arup Kumar Gope <gope.aust@gmail.com>
2020-12-07 20:55:45 +0100

    Hello world with Class

---
+++ MyWork/Hello World with Class/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hello_World_with_Class
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Getname()
+
+        { return Name;} }
+}
---
+++ MyWork/Hello World with Class/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Hello_World_with_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person=new Person();
+            person.Name = "Arup Kumar Gope";
+            Console.WriteLine(person.Getname());
+                
+        }
+    }
+}
