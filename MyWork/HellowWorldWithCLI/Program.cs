f21f3de18d972b9c550da1f753f70306898f77db
Ishfaq Ahmad Jadoon <ashfaq.khan2235@gmail.com>
2020-12-07 02:18:32 +0100

    HelloWorldWithCLI

---
+++ MyWork/HellowWorldWithCLI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HellowWorldWithCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
