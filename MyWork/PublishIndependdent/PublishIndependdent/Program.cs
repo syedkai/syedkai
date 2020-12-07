cdfc36e1a0dba7fd7f53195fc5cabd78c0a828df
Kanav <apple@Kanavs-MacBook-Air.local>
2020-12-07 21:00:32 +0100

    E5 done

---
+++ MyWork/PublishIndependdent/PublishIndependdent/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace PublishIndependdent
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("NET 5.0");
+
+            int a = 5;
+
+            for(int i=0; i < a; i++) 
+            {
+                Console.WriteLine($"Counter: {i}");
+
+                await Task.Delay(500);
+            }
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+        }
+    }
+}
