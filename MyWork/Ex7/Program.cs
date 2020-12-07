fc3604e703c67aa1152177fa426a0de612ba2576
LAPTOP-NIFSKO5C\49176 <suraiyarahman9808@gmail.com>
2020-12-07 23:34:35 +0100

    Exercise 7

---
+++ MyWork/Ex7/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConfigurationBuilder me = new ConfigurationBuilder();
+            me.AddJsonFile("appsettings.json");
+            me.SetBasePath(System.IO.Directory.GetCurrentDirectory());
+            IConfigurationRoot cr = me.Build();
+            Console.WriteLine(cr["Name"]);
+            Console.WriteLine(cr["Nationality"]);
+            Console.WriteLine(cr["Profession"]);
+        }
+    }
+}
