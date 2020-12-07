39745f3db36a5ad149fd170fd5cc7fecb1cf96ec
Ishfaq Ahmad Jadoon <ashfaq.khan2235@gmail.com>
2020-12-07 19:28:30 +0100

    HelloWorldWithClass

---
+++ MyWork/Exercise 4/Exercise 4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            biodata biodata = new biodata();
+
+            biodata.Name = "Ishfaq Ahmad Jadoon";
+            biodata.Age = 25;
+
+            biodata.greeting();
+
+        }
+    }
+}
