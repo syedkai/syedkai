75b155e8e54e70cc118291f01f7e2dcfe118e08f
Joseph Itopa <grabjosphab@gmail.com>
2020-12-07 21:36:43 +0100

    solution to publish-independent

---
+++ MyWork/Excercise_5/publish-independent/publish-independent/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace publish_independent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World for publish independent!");
+        }
+    }
+}
