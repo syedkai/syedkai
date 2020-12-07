a5ad6e26d68e65abd100c9d98bfab661bff46e96
sweta13 <sweta.swain@stud.fra-uas.de>
2020-12-07 18:49:57 +0100

    Changes in VS

---
+++ MyWork/HelloWorldWithVS/HelloWorldwithVS/HelloWorldwithVS/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorldwithVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("The VS type GIT push");
+        }
+    }
+}
