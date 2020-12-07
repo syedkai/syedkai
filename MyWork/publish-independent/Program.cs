c884a7d76865d54da26f8b78a197cc2c61c13b2a
Janet007 <root@Janets-MacBook-Pro.local>
2020-12-07 23:57:09 +0100

    Exercise E5 Independent Framework Build

---
+++ MyWork/publish-independent/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace publish_independent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Build the app with diff runtimeidentifiers");
+        }
+    }
+}
