ac15d7f880c0a5af75295449a1d8cb20f8b4cca7
Ishfaq Ahmad Jadoon <ashfaq.khan2235@gmail.com>
2020-12-07 02:12:54 +0100

    Exercise 4 (Ex 1,2,3,4 Finished Did in 2019 also CLI exercise was in 2019 )

---
+++ MyWork/Exercise 4/Biodata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercise_4
+{
+    public class Biodata
+    {
+        public String Name { get; set; }
+        public int Age { get; set; }
+        public String Getnameandage()
+            {
+            return Name + " " + Age;
+            }
+
+    }
+
+}
---
+++ MyWork/Exercise 4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercise_4
+    
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Biodata biodata = new Biodata();
+            biodata.Name = "Ishfaq Ahmad Jadoon";
+            biodata.Age = 36;
+            String Nameandage=biodata.Getnameandage();
+            
+            Console.WriteLine("Hello World! and " + Nameandage );
+        }
+    }
+}
