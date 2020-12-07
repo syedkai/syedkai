cb458830ae0366a238714092d8de2b91d56ab134
Md Nabuat Al Jahid <nabuat.aljahid@gmail.com>
2020-12-07 16:35:57 +0100

    Exercise 2,3 and 4 updated

---
+++ MyWork/Ex4/Class.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex4
+{
+    class Substraction
+    {
+        public int Sub (int a, int b) //funcion of class
+        {
+            return a - b; //substraction of the class
+        }
+    }
+}
---
+++ MyWork/Ex4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)//main method
+        {
+            int a, b, d; //calling variable
+            Console.WriteLine("Hello World with a new class!"); //print line
+            Console.WriteLine("..."); // do
+            Console.WriteLine("Enter your first value for substraction:"); //do
+            a = int.Parse(Console.ReadLine()); //storing input value
+            Console.WriteLine("Enter your second value:"); //print line
+            b = int.Parse(Console.ReadLine()); //storing input value
+            Substraction c = new Substraction (); //memory allocation
+            d = c.Sub(a, b); //substraction of return value
+            Console.WriteLine("Your result:" + d); //print the return value
+            Console.ReadLine();
+
+        }
+    }
+}
