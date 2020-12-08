66e9bdc766fda63cbc84e2c7f51add39bc9724b9
Vivek Purohit <purohitvivek7@gmail.com>
2020-12-08 16:35:51 +0530

    Working task 4

---
+++ MyWork/HelloWorldCli/Ex4/Ex4/PersonDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Person
+{
+
+    public string name;
+    public int age;
+    public bool hasPet;
+
+    public void Greeting()
+    {
+
+        Console.WriteLine("Hi, My name is " + name + "and my age is " + age);
+
+    }
+}
---
+++ MyWork/HelloWorldCli/Ex4/Ex4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            person.name = "Vivek"
+            person.age = 26
+            person.hasPet = true;
+
+            person.Greeting();
+        }
+    }
+}
