6305c69303c29c747ace8961d9c2c0302e6cab48
Haritha G <harytha6@gmail.com>
2020-12-08 23:00:38 +0100

    NuGet copied and packed from local source

--- MyWork/Haritha/E10 - WorkingwithNuGet/E10 - WorkingwithNuGet/Program.cs
+++ MyWork/Haritha/E10 - WorkingwithNuGet/E10 - WorkingwithNuGet/Program.cs
@@ -11,19 +11,17 @@ namespace E10___WorkingwithNuGet
         {
             Console.WriteLine("Hello I have added few nice" +
                 " functionalities from NuGet packages!");
-            /* added accounting.js through packet manager in VS
-             
-             added humanizer through Command Line Interface 
-            eg : dotnet add package Humanizer --version 2.8.26 
             
-            deleted accounting.js through editing .csproj file*/
-
             int userinput;
             float value;
+
+            // NuGet package copied and packed from local library
             Console.WriteLine("Enter value in milliseconds and I will make it easier for you");
             userinput = int.Parse(Console.ReadLine());
             Console.WriteLine(TimeSpan.FromMilliseconds(userinput).Humanize(5));
             // humanize (5) represents no. of units to describe
+
+
 
             // Referencing from Own Local Library
             // add existing library to solution and add project reference
@@ -31,10 +29,17 @@ namespace E10___WorkingwithNuGet
             value = float.Parse(Console.ReadLine());
 
             // Creating instance of class from Local Library
-            MyMathLib myMathLib = new MyMathLib();
-            MyMathLib MathLibinstance = myMathLib;
+            MyMathLib MathLibinstance = new MyMathLib();
+            // also can give MyMathLibrary.MyMath........
             Console.WriteLine(MathLibinstance.SquareRoot(value));
+
+
+            /* added accounting.js through packet manager in VS
+             
+             added humanizer through Command Line Interface 
+            eg : dotnet add package Humanizer --version 2.8.26 
             
+            deleted accounting.js through editing .csproj file*/
         }
     }
 }
