f81fddc71e35c805181f8b26da163f544ce727e3
SHIVAM KAUSHIK <shivamkaushik007@yahoo.com>
2020-12-09 21:20:51 +0530

    Implementation of Interface is done along with classes

---
+++ MyWork/InterfaceImplementation/InterfaceTrial2/Result.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceTrial2
+{
+    
+    class Result: IMyInterface2
+
+    {
+        int sum;
+        double average;
+        public void train2(int[] data)
+
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                sum += data[i];
+            }
+
+            average = sum / data.Length;
+
+
+        }
+
+
+
+    }
+}
