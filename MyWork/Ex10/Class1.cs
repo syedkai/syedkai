c83317bb2fade691c40ff9ecd9d1af6cd7d658df
LAPTOP-NIFSKO5C\49176 <suraiyarahman9808@gmail.com>
2020-12-10 23:16:09 +0100

    Exercise 10

---
+++ MyWork/Ex10/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex11
+{
+   public class Class1
+    {
+        int[] ABC;
+        public Class1(int[] XYZ) //constructor should be AS a Class1 name here Class1 name from class is using as a constructor
+        {
+            ABC = XYZ;
+        }
+
+        public void binarySort()
+        {
+            for (int i = 0; i < ABC.Length - 1; i++)
+                for (int j = i + 1; j < ABC.Length; j++)
+                    if (ABC[i] > ABC[j])
+                    {
+                        int temp = ABC[i];
+                        ABC[i] = ABC[j];
+                        ABC[j] = temp;
+                    }
+        }
+    }
+}
---
+++ MyWork/Ex10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ex10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] EFG = { 30, 31, 2, 33, -1};
+            Class1 XYZ = new Class1(EFG);
+            XYZ.binarySort();
+            for (int i=0; i<5; i++)
+            {
+                Console.WriteLine(EFG[i]);
+            }
+            //Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
