85b5d87c297c0280c9c10e1e0cd5ae3c41d7812e
Al Imran Ahmed <al.imran.cse@gmail.com>
2020-12-10 19:52:29 -0800

    #E4: Project dependency tried out

---
+++ MyWork/HelloWorldWithClass/HelloWorldWithClass/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SpeakerClass;
+
+namespace HelloWorldWithClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Speaker speaker = new Speaker();
+            speaker.speak();
+        }
+    }
+}
---
+++ MyWork/HelloWorldWithClass/SpeakerClass/Speaker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpeakerClass
+{
+    public class Speaker
+    {
+        static void Main(string[] args)
+        {
+            ;
+        }
+
+        public void speak()
+        {
+            Console.WriteLine("Speaking from speaker class");
+        }
+    }
+}
