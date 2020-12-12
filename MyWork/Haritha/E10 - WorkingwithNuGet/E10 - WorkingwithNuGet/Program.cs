86a8b7b9e7aadbef2ace2cff9be4f2bc38c00fd5
Haritha G <harytha6@gmail.com>
2020-12-12 15:22:42 +0100

    Own nuget package referenced from nuget.org and published updated version .nupkg

--- MyWork/Haritha/E10 - WorkingwithNuGet/E10 - WorkingwithNuGet/Program.cs
+++ MyWork/Haritha/E10 - WorkingwithNuGet/E10 - WorkingwithNuGet/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-// using MyMathLibrary;
 using Humanizer;
-
+using Librarynuget;
+using MyMathLibrary;
 
 namespace E10___WorkingwithNuGet
 {
@@ -11,34 +11,37 @@ namespace E10___WorkingwithNuGet
         {
             Console.WriteLine("Hello I have added few nice" +
                 " functionalities from NuGet packages!");
-            
+
             int userinput;
             float value;
+            string hexinput,binoutput; 
 
-            // NuGet package copied and packed from local library
+            // NuGet package - created and uploaded to nuget.org- referencing online
+            Console.WriteLine("Enter hexadecimal string for conversion to binary");
+            hexinput = Console.ReadLine();
+            CanonicalForm hexbinary = new CanonicalForm();
+            binoutput = hexbinary.HexStringToBinary(hexinput);
+            Console.WriteLine("The result is :" + binoutput );
+
+            // NuGet package copied and packed from local source - offline
             Console.WriteLine("Enter value in milliseconds and I will make it easier for you");
             userinput = int.Parse(Console.ReadLine());
             Console.WriteLine(TimeSpan.FromMilliseconds(userinput).Humanize(5));
             // humanize (5) represents no. of units to describe
 
 
-
-            // Referencing from Own Local Library
-            // add existing library to solution and add project reference
             Console.WriteLine("Enter a value and I will give you its square root");
             value = float.Parse(Console.ReadLine());
 
             // Creating instance of class from Local Library
-         //  MyMathLib MathLibinstance = new MyMathLib();
+              MyMathLib MathLibinstance = new MyMathLib();
             // also can give MyMathLibrary.MyMath........
-        //    Console.WriteLine(MathLibinstance.SquareRoot(value));
-            
-            /* added accounting.js through packet manager in VS
+              Console.WriteLine(MathLibinstance.SquareRoot(value));
              
-             added humanizer through Command Line Interface 
-            eg : dotnet add package Humanizer --version 2.8.26 
+           //  added humanizer through Command Line Interface 
+           // eg : dotnet add package Humanizer --version 2.8.26 
             
-            deleted accounting.js through editing .csproj file*/
+          
         }
     }
 }
---
+++ MyWork/Haritha/Librarynuget/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace HextoBinary
+{
+    public class CanonicalForm
+    {
+        // edited and published new version of package
+        private static readonly System.Collections.Generic.Dictionary<char, string> hexCharacterToBinary = new System.Collections.Generic.Dictionary<char, string>
+        {
+                { '0', "0000" },
+                { '1', "0001" },
+                { '2', "0010" },
+                { '3', "0011" },
+                { '4', "0100" },
+                { '5', "0101" },
+                { '6', "0110" },
+                { '7', "0111" },
+                { '8', "1000" },
+                { '9', "1001" },
+                { 'a', "1010" },
+                { 'b', "1011" },
+                { 'c', "1100" },
+                { 'd', "1101" },
+                { 'e', "1110" },
+                { 'f', "1111" }
+        };
+
+        public string HexStringToBinary(string hex)
+        {
+            StringBuilder result = new StringBuilder();
+            foreach (char c in hex)
+            {
+                // This will crash for non-hex characters. You might want to handle that differently.
+                result.Append(hexCharacterToBinary[char.ToLower(c)]);
+            }
+            return result.ToString();
+        }
+    }
+}
