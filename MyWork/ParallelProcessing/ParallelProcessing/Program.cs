d730eb4111234f29f7d1474a4e794ca2b99948b6
Sabin Bajracharya <sabin.bajracharya@gmail.com>
2020-12-13 22:45:10 +0100

    Add: New Project/Solutions for Prallel Processing

---
+++ MyWork/ParallelProcessing/ParallelProcessing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ParallelProcessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
