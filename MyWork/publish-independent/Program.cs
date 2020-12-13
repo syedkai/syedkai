3f308263195e8f4c9f7e23de58b2c855bbe1442d
Sourav <sourav.sumit@stud.fra-uas.de>
2020-12-14 00:12:47 +0600

    Added a project named publish-independent

---
+++ MyWork/publish-independent/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace publish_independent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a publish-independent Project.");
+            Console.ReadLine();
+        }
+    }
+}
