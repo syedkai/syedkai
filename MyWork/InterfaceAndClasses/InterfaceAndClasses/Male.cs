c7d226a9386e9f7f2626cbede5d1764b8ad7e797
Sabin Bajracharya <sabin.bajracharya@gmail.com>
2020-12-13 22:28:37 +0100

    Add: Implement IPerson in Male class and overried the methods

---
+++ MyWork/InterfaceAndClasses/InterfaceAndClasses/Male.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAndClasses
+{
+    class Male : IPerson
+    {
+        public void hair()
+        {
+            Console.WriteLine("Male usually have short hair");
+        }
+
+        public void height()
+        {
+            Console.WriteLine("Male are usually taller.");
+        }
+
+        public void voice()
+        {
+            Console.WriteLine("Male usually have husky voice");
+        }
+    }
+}
