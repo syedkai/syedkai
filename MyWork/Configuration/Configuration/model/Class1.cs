a0b9accb68fc34bd779a911c5d1de92bf9692d4c
Sabin Bajracharya <sabin.bajracharya@gmail.com>
2020-12-13 21:36:42 +0100

    Add: Model file to map with json

---
+++ MyWork/Configuration/Configuration/model/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Configuration.model
+{
+    class UserData
+    {
+        public string Name { get; set; }
+        public string Dob { get; set;}
+
+        public UserData(string name, string dob)
+        {
+            Name = name;
+            Dob = dob;
+        }
+    }
+}
