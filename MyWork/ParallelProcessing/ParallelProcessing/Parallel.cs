76405498e3f37fb8bf8c873d0cfd81ea472572b5
Sabin Bajracharya <sabin.bajracharya@gmail.com>
2020-12-13 23:54:37 +0100

    Add: Each worker with its own thread

--- MyWork/ParallelProcessing/ParallelProcessing/Parallel.cs
+++ MyWork/ParallelProcessing/ParallelProcessing/Parallel.cs
@@ -6,6 +6,8 @@ namespace ParallelProcessing
     class Parallel : IThread
     {
 
+        private long finishedCounter = 0;
+
         public void sequenceTask(int threadLoop, Action<object> callback)
         {
             Thread.CurrentThread.Name = "Single Thread";
@@ -16,12 +18,33 @@ namespace ParallelProcessing
 
         public void ownThreadTask(int numOfThread, Action<object> callback)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < numOfThread; i++)
+            {
+                var thread = new Thread(new ParameterizedThreadStart(callback));
+                thread.Name = i.ToString();
+                thread.Start(new Action<string>(onThreadFinished));
+            }
+
+            while (true)
+            {
+                if (Interlocked.Read(ref finishedCounter) == numOfThread)
+                {
+                    break;
+                } else
+                {
+                    Thread.Sleep(1000);
+                }
+            }
         }
 
         public void parallelTask(int numOfThread, Action<object> callback)
         {
             throw new NotImplementedException();
+        }
+
+        private void onThreadFinished(string threadName)
+        {
+            Interlocked.Increment(ref finishedCounter);
         }
     }
 }
