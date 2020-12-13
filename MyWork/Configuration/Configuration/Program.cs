311137489973559fca356cda8d6b47069756af4d
Sabin Bajracharya <sabin.bajracharya@gmail.com>
2020-12-13 22:16:44 +0100

    Add: Beautify output

--- MyWork/Configuration/Configuration/Program.cs
+++ MyWork/Configuration/Configuration/Program.cs
@@ -16,19 +16,30 @@ namespace Configuration
                 .AddEnvironmentVariables()
                 .Build();
 
+            Console.WriteLine("Reading config from cmd Argument");
             var cmdArgLocation = config["location"];
             Console.WriteLine("{0}", cmdArgLocation);
+            
+            Console.WriteLine("----------------------------------------------------------");
 
+            Console.WriteLine("Reading config from appsettings.json by using key.");
             var username = config["username"];
             var mode = config["mode"];
-
             Console.WriteLine("Username={0} .. Mode={1}", username, mode);
 
+            Console.WriteLine("----------------------------------------------------------");
+
+            Console.WriteLine("Reading config from appsettings.json (using Typed Settings)");
             UserData userData = new UserData();
             config.GetSection("data").Bind(userData);
             Console.WriteLine("Name={0} .. DOB={1}", userData.Name, userData.Dob);
 
+
+            Console.WriteLine("----------------------------------------------------------");
+
+
             // Read Array from json file
+            Console.WriteLine("Reading array of strings from appsettings.json and showing them.");
             var scopes = config.GetSection("scope").Get<string[]>();
 
             Console.WriteLine("Scopes-------");
