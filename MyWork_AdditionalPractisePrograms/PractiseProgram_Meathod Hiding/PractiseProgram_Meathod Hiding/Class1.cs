05c44b13a83f273aad9e9a1ef430d178b64d6a88
SHIVAM KAUSHIK <shivamkaushik007@yahoo.com>
2020-12-13 19:59:30 +0530

    Demonstration of Meathod Hiding is Done

---
+++ MyWork_AdditionalPractisePrograms/PractiseProgram_Meathod Hiding/PractiseProgram_Meathod Hiding/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractiseProgram_Meathod_Hiding
+{
+    class Class1:Program
+    {
+
+        public new void test()
+        {
+
+            Console.WriteLine("I am child Class");
+        }
+
+        public void Parentclass()
+
+            {
+
+            base.test();
+        
+        }
+
+        static void Main()
+
+
+        {
+            Class1 obj = new Class1();
+            obj.test();
+            obj.Parentclass();
+            Program p;
+            p = obj;
+            p.test();
+
+        }
+    }
+}
---
+++ MyWork_AdditionalPractisePrograms/PractiseProgram_Meathod Hiding/PractiseProgram_Meathod Hiding/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractiseProgram_Meathod_Hiding
+{
+    class Program
+    {
+
+
+        public void test ()
+        {
+
+            Console.WriteLine("I am Parent Class");
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
