c83a73ad0cb8eac8d0e39f9205fa0fc8c95ad89f
Sudhakar3377 <sudhakarvundavalli00@gmail.com>
2020-12-13 18:53:11 +0100

    E5 and E6 done

---
+++ MyWork/E5 and E6/Independent/Independent/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Independent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
