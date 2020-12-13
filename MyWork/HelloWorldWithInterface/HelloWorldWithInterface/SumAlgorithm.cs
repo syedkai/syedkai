7704b38668901dec0a6b01da13afe8a95a64c006
paweenp <paween.pongsomboon@gmail.com>
2020-12-13 20:04:20 +0100

    add Exercise 8 HelloWorldWithInterface to branch

---
+++ MyWork/HelloWorldWithInterface/HelloWorldWithInterface/SumAlgorithm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace HelloWorldWithInterface
+{
+    public class SumAlgorithm : IMyInterface
+    {
+        private double sum;
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        public SumAlgorithm()
+        {
+            this.sum = 0;
+        }
+
+        /// <summary>
+        /// return sum value
+        /// </summary>
+        /// <returns></returns>
+        public double GetResult()
+        {
+            return this.sum;
+        }
+
+        /// <summary>
+        /// implement train with sum algorithm
+        /// </summary>
+        /// <param name="data"></param>
+        public void Train(double[] data)
+        {
+            foreach(var number in data)
+            {
+                this.sum += number;
+            }
+        }
+
+        public void Save()
+        {
+
+        }
+
+        public void Load()
+        {
+
+        }
+
+    }
+}
