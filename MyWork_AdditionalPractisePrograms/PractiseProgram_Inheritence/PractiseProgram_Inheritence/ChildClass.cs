f5742e986df679d93c2469d62e0c52fa16e49eed
SHIVAM KAUSHIK <shivamkaushik007@yahoo.com>
2020-12-13 19:56:39 +0530

    Demonstration of Inheritence is done

---
+++ MyWork_AdditionalPractisePrograms/PractiseProgram_Inheritence/PractiseProgram_Inheritence/ChildClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractiseProgram_Inheritence
+{
+    class ChildClass : ParentClass
+    {
+        public ChildClass():base(50)
+
+        {
+
+            Console.WriteLine("Child Class constructor is called");
+        }
+
+        public void test()
+
+        {
+
+
+            Console.WriteLine("Test Meathod is called");
+        }
+
+
+
+        static void Main()
+
+        {
+            ChildClass obj = new ChildClass();
+            obj.i = 5;
+            Console.WriteLine(obj.i);
+        }
+
+    }
+}
---
+++ MyWork_AdditionalPractisePrograms/PractiseProgram_Inheritence/PractiseProgram_Inheritence/ParentClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractiseProgram_Inheritence
+{
+
+    
+    class ParentClass
+    {
+
+        public int i;
+
+        public ParentClass(int x)
+
+            {
+            Console.WriteLine("Parent Class constructor is calledamd the vslue of x is " +x);
+
+           }
+
+        public void print()
+        {
+
+            Console.WriteLine("Print meathod is called ");
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
