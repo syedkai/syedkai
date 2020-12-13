5b0fca8567b85e7b94c3b85662d973ba3cd878b6
Sabin Bajracharya <sabin.bajracharya@gmail.com>
2020-12-13 22:26:22 +0100

    Add: IPerson intefrace class with some methods

---
+++ MyWork/InterfaceAndClasses/InterfaceAndClasses/IPerson.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAndClasses
+{
+    interface IPerson
+    {
+        void voice();
+        void hair();
+        void height();
+    }
+}
