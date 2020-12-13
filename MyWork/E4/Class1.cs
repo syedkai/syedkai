aeeb1e6f74160fa0fd02ff95728626a637660894
Sudhakar3377 <sudhakarvundavalli00@gmail.com>
2020-12-13 18:46:53 +0100

    E4 done

---
+++ MyWork/E4/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorldwithVS
+{
+    class Class1
+    {
+        public int Calculation(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
