132a0c4720b1931d3bce15d811007baf6fa381b4
Sabin Bajracharya <sabin.bajracharya@gmail.com>
2020-12-13 22:30:22 +0100

    Add: Implement IPerson in Female class and overried the methods

---
+++ MyWork/InterfaceAndClasses/InterfaceAndClasses/Female.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAndClasses
+{
+    class Female: IPerson
+    {
+        public void hair()
+        {
+            Console.WriteLine("Female usually have longer and shiny hair");
+        }
+
+        public void height()
+        {
+            Console.WriteLine("FeMale are usually shorter or average in terms of height.");
+        }
+
+        public void voice()
+        {
+            Console.WriteLine("Female usually have beautiful and soft voice");
+        }
+    }
+}
