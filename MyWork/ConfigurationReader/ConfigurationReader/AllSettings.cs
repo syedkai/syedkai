6ee93b50747ab7fcbf6db761796746313dee690f
isabeen <khannssabeen@gmail.com>
2020-12-13 22:52:13 +0500

    Exercise 7

---
+++ MyWork/ConfigurationReader/ConfigurationReader/AllSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigurationReader
+{
+    class AllSettings
+    {
+        public string welcomeText { get; set; }
+
+        public int age { get; set; }
+
+        public float result { get; set; }
+    }
+}
---
+++ MyWork/ConfigurationReader/ConfigurationReader/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace ConfigurationReader
+{
+    /// <summary>
+    /// Reading Configuration, Reading Files, Reading file
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true) //change appsettings.json file settings to 'copy if newer'
+            .AddCommandLine(args)
+            .AddEnvironmentVariables();
+
+
+            IConfigurationRoot configuration = builder.Build();
+
+            //following are coming from command line
+            var color = configuration["color"];
+            var fontSize = configuration["fontSize"];
+
+            //following are coming from appsettings.json
+            var welcomeText = configuration["welcomeText"];
+            var age = configuration["age"];
+            var result = configuration["result"];
+
+            //reading environment variables
+            var userName = configuration["USERNAME"];
+
+            //loading settings
+            AllSettings allSettings = new AllSettings();
+            configuration.Bind(allSettings);
+
+            Console.WriteLine($"{welcomeText} {userName}");
+            Console.WriteLine($"Your age is {age}");
+            Console.WriteLine($"Your result is {result} %");
+            Console.ReadLine();
+
+        }
+    }
+}
