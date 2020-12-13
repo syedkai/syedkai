94925edec640dc6fbb25195a22edc89b3f4dfe59
SHIVAM KAUSHIK <shivamkaushik007@yahoo.com>
2020-12-13 19:57:35 +0530

    Demonstration of Meathod Overriding is done

---
+++ MyWork_AdditionalPractisePrograms/PractiseProgram_MeathodOverriding/PractiseProgram_MeathodOverriding/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractiseProgram_MeathodOverriding
+{
+    class Class1: Program
+    {
+        // we can also use new intead of override in that case virtual is not used in parent class
+        public override void test2()
+        {
+
+            Console.WriteLine("child");
+
+        }
+
+
+       
+
+        static void Main()
+
+        {
+            Program p;
+            Class1 obj = new Class1();
+            p = obj;
+            obj.test2();
+            p.tes1();
+        }
+
+    }
+}
---
+++ MyWork_AdditionalPractisePrograms/PractiseProgram_MeathodOverriding/PractiseProgram_MeathodOverriding/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractiseProgram_MeathodOverriding
+{
+    class Program 
+    {
+
+
+        public void tes1()
+
+        {
+
+            Console.WriteLine("parent");
+        }
+
+        public virtual void test2()
+
+        {
+
+
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            
+
+        }
+    }
+}
