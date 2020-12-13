a9d7d9bd6a8892346e3285ea1d418941cba7b178
Sabin Bajracharya <sabin.bajracharya@gmail.com>
2020-12-13 23:29:44 +0100

    Add: Show elpased time of sequence thread

--- MyWork/ParallelProcessing/ParallelProcessing/Program.cs
+++ MyWork/ParallelProcessing/ParallelProcessing/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading;
 
 namespace ParallelProcessing
@@ -8,8 +9,14 @@ namespace ParallelProcessing
         static void Main(string[] args)
         {
 
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
             Parallel parallel = new Parallel();
             parallel.sequence(5, callback);
+
+            stopWatch.Stop();
+            Console.WriteLine("Total Elapsed Time = {0}", stopWatch.Elapsed);
 
             Console.ReadLine();
         }
@@ -20,7 +27,7 @@ namespace ParallelProcessing
             Console.WriteLine("{0} : Start Thread name {1}", Thread.CurrentThread.ManagedThreadId,Thread.CurrentThread.Name);
             Console.ResetColor();
 
-            long count = 9999;
+            long count = 99999999;
             long total = 0;
             for (int i = 0; i < count; i++) {
                 total += (i + 1) * count / 50*16;
@@ -31,7 +38,7 @@ namespace ParallelProcessing
                 ((Action<String>) onComplete)(Thread.CurrentThread.Name);
             }
 
-            Console.ForegroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("{0} : Stop Thread name {1}", Thread.CurrentThread.ManagedThreadId, Thread.CurrentThread.Name);
             Console.ResetColor();
         }
