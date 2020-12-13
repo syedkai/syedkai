a1f2ce7e06ad0b1f6ba702684ab983a39e6e6bdf
Sabin Bajracharya <sabin.bajracharya@gmail.com>
2020-12-13 22:54:56 +0100

    Add: Implement IThread

---
+++ MyWork/ParallelProcessing/ParallelProcessing/Parallel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParallelProcessing
+{
+    class Parallel : IThead
+    {
+        public void sequence()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
