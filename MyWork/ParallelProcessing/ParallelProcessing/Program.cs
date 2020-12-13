758969ebe9830ee73666a2039c7c4c550253e020
Sabin Bajracharya <sabin.bajracharya@gmail.com>
2020-12-13 23:17:40 +0100

    Add: Run loop in a single thread

--- MyWork/ParallelProcessing/ParallelProcessing/Program.cs
+++ MyWork/ParallelProcessing/ParallelProcessing/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace ParallelProcessing
 {
@@ -6,7 +7,29 @@ namespace ParallelProcessing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Parallel parallel = new Parallel();
+            parallel.sequence(5, callback);
+
+            Console.ReadLine();
+        }
+
+        private static void callback(object onComplete)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Thread ${0}", Thread.CurrentThread.Name);
+
+            long count = 9999;
+            long total = 0;
+            for (int i = 0; i < count; i++) {
+                Console.WriteLine(i);
+                total += (i + 1) * count / 50*16;
+            }
+
+            if (onComplete != null)
+            {
+                ((Action<String>) onComplete)(Thread.CurrentThread.Name);
+            }
         }
     }
 }
