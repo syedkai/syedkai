3610d69daa56eb8759205b2eed372059d61eb37b
Ishfaq Ahmad Jadoon <ashfaq.khan2235@gmail.com>
2020-12-14 17:16:46 +0100

    Exericise 5

---
+++ MyWork/Ex 7/Ex 7/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using Microsoft.Extensions.Configuration.CommandLine;
+
+namespace ConsoleApp1
+
+{
+    class Program
+    {
+        private static ConfigurationBuilder m_Builder;
+        private static IConfigurationRoot m_Config;
+        // private static object appsettings;
+
+        public static void Main(string[] args)
+        {
+            var m_Builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables();
+
+            if (args != null)
+            {
+                m_Builder.AddCommandLine(args);
+            }
+
+
+            m_Config = m_Builder.Build();
+            Console.WriteLine(m_Config["Setting1"]);
+            Console.WriteLine(m_Config["Setting2"]);
+
+        }
+
+
+    }
+}
