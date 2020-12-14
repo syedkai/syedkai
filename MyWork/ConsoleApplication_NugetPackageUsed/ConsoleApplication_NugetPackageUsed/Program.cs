5b2775e7807202b6a5bd245d22a6630ac00e05e7
SHIVAM KAUSHIK <shivamkaushik007@yahoo.com>
2020-12-14 17:17:24 +0530

    NuGet Library created and used in a Console Application.

---
+++ MyWork/ConsoleApplication_NugetPackageUsed/ConsoleApplication_NugetPackageUsed/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NuGetPackage_ConsoleLibrary;
+
+
+namespace ConsoleApplication_NugetPackageUsed
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Addition of Two double type Number is:");
+           double Result = Class1.test1(525.254,786.879);
+
+            Console.WriteLine(Result);
+
+            
+
+
+
+           
+        }
+    }
+}
---
+++ MyWork/NuGetPackage_ConsoleLibrary/NuGetPackage_ConsoleLibrary/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NuGetPackage_ConsoleLibrary
+{
+    public class Class1
+    {
+
+
+        public static double test1(double x, double y)
+
+        {
+            return x + y;
+
+
+        }
+    }
+}
