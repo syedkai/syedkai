ab7d386e445e14885967d460a686908b0443bb28
Vivek Purohit <purohitvivek7@gmail.com>
2020-12-14 22:01:39 +0530

    Ex4 working properly

---
+++ MyWork/HelloWorldCli/Ex4/Ex4/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex4
+{
+    public class Student
+    {
+
+        string name;
+        string age;
+
+
+        public void studentInfo() {
+
+            Console.WriteLine("Name: " +name , "Age: " +age);
+        }
+    }
+}
