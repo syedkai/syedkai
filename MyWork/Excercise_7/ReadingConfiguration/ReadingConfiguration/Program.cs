12359d2fb38711bc1b69e8bbf7b1da79828b943f
Joseph Itopa <grabjosphab@gmail.com>
2020-12-14 11:24:54 +0100

    Reading Configuration, Files

---
+++ MyWork/Excercise_7/ReadingConfiguration/ReadingConfiguration/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace ReadingConfiguration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World for Knugget Package Manager!");
+
+            var builder = new ConfigurationBuilder()
+           .SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: false, reloadOnChange: true).AddEnvironmentVariables();
+            if (args != null)
+            {
+                builder.AddCommandLine(args);
+            }
+
+            IConfigurationRoot configuration = builder.Build();
+            // Following are comming from command line.
+            var color = configuration["color"];
+            var fontSize = configuration["fontSize"];
+
+        }
+    }
+}
