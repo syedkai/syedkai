a1b44659c21555f4584619a7aa5f641b315c1181
paweenp <paween.pongsomboon@gmail.com>
2020-12-15 08:43:49 +0100

    add Exercise 9 HelloWorldWithParallel to branch (unfinished)

---
+++ MyWork/HelloWorldWithParallel/HelloWorldWithParallel/ParallelClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HelloWorldWithParallel
+{
+    class ParallelClass
+    {
+        private long m_FinishCounter = 0;
+
+        /// <summary>
+        /// Execute Job after Job as sequence
+        /// </summary>
+        /// <param name="sequences"></param>
+        /// <param name="func"></param>
+        public void StartJob(int sequences, Action<object> func)
+        {
+            Thread.CurrentThread.Name = "SingleThread";
+
+            for(int i = 0; i < sequences; i++)
+            {
+                func(null);
+            }
+
+        } 
+
+        /// <summary>
+        /// Execute each job in its own Thread
+        /// </summary>
+        public void StartJobInItsThread()
+        {
+
+        }
+
+        /// <summary>
+        /// Execute Jobs in parallel
+        /// </summary>
+        public void StartJobParallel()
+        {
+
+        }
+}
---
+++ MyWork/HelloWorldWithParallel/HelloWorldWithParallel/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HelloWorldWithParallel
+{
+    class Program
+    {
+        static int numThreads = 100;
+        // public static int rand = new Random();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This a demonstration of Threading by compared methods with Threading and without Threading.");
+
+            Console.WriteLine("Input your expected Thead number : ");
+            numThreads = Console.Read();
+
+            Console.WriteLine("Press any key to start");
+            Console.ReadLine();
+
+
+            int loop = 20;
+
+            double[] listSingleThread = new double[loop];
+            double[] listMultiThread = new double[loop];
+
+            // timer
+            Stopwatch sw = new Stopwatch();
+
+            ParallelClass task = new ParallelClass();
+
+
+            for (int i = 0; i<loop; i++)
+            {
+                // 1. Single Thread
+                sw.Start();
+
+                task.StartSequence(numThreads, workerFunction);
+
+                sw.Stop();
+
+                listSingleThread[i] = sw.ElapsedMilliseconds;
+
+                // 2. Multi Thread Native 1
+                //sw.Start();
+
+                //task.StartMultithreadedNative(numThreads, workerFunction);
+
+                //sw.Stop();
+
+                // 3. Multi Thread Native 2
+                //sw.Start();
+
+                //task.StartMultithreadedNativeV2(numThreads, workerFunction);
+
+                //sw.Stop();
+
+                // 4. Multi Thread Native 3
+                sw.Start();
+
+                task.StartWithTpl(numThreads, workerFunction);
+
+                sw.Stop();
+
+                listMultiThread[i] = sw.ElapsedMilliseconds;
+
+            }
+
+           
+
+            //Console.ForegroundColor = ConsoleColor.Magenta;
+            //Console.WriteLine($"time used : {sw.ElapsedMilliseconds} ms");
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// run a random function
+        /// </summary>
+        /// <param name="OnfinishDelegate"></param>
+        private static void workerFunction(object OnfinishDelegate)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine($"Started thread: {Thread.CurrentThread.Name} - {Thread.CurrentThread.ManagedThreadId}");
+
+            double r = 350000000000;
+            int loop = 20000000;
+            double factor = 1.5;
+
+            for (int i = 1; i < loop; i++)
+                r += r* factor;
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Stop Thread: {0}", Thread.CurrentThread.Name);
+        }
+
+        public static Task<int> AddAsynce(int i, int j)
+        {
+            var result = Task<int>.Run(() =>
+            {
+                return i + j;
+            });
+
+            return result;
+        }
+
+        public static async Task DelaySomething()
+        {
+            await new Task(() =>
+            {
+                Task.Delay(5000).Wait();
+            });
+
+            await Task.Run(() =>
+            {
+                Task.Delay(5000).Wait();
+            });
+        }
+    }
+}
