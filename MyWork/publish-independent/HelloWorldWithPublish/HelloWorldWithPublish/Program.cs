59e24222c14f05d65195fab867dac0dc60309ca3
SnigdhaKantam <snigdhakatm@gmail.com>
2020-12-15 22:45:48 +0530

    Publish task with dependent and self-contained

---
+++ MyWork/publish-independent/HelloWorldWithPublish/HelloWorldWithPublish/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace HelloWorldWithPublish
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            for(int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Counting {0}", i);
+                Task.Delay(1000);
+            }
+
+
+        }
+    }
+}
