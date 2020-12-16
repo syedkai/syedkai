c4bc430291c04603165b120b35a444aafd5967d9
Usama Shakoor <usama.shakoor10@gmail.com>
2020-12-16 23:51:28 +0100

    Calculator code
    Calculator code and Calculation library

---
+++ MyWork/Calculator/Calculation/Basic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Calculation
+{
+    public static class basic
+    {
+        public static double Ddivide(int a,int b) {
+            return (double)a / (double)b;
+        }
+        public static double Mmultiply(int a,int b) {
+            return (double)a * (double)b;
+        }
+        public static double Aadd(int a, int b) {
+            return (double)a + (double)b;
+        }
+        public static double Ssubtract(int a, int b) {
+            return (double)a - (double)b;
+        }
+    }
+}
---
+++ MyWork/Calculator/Calculator/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Calculation;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World, this is my basic calculator! What operation do you want,choose number? ");
+            Console.WriteLine("1 Divide");
+            Console.WriteLine("2 Multiply");
+            Console.WriteLine("3 Add");
+            Console.WriteLine("4 Subtract");
+            var option=Console.ReadLine();
+            
+            switch (int.Parse(option)) {
+                case 1:
+                    Console.WriteLine("Type Arg1");
+                    int a = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Type Arg2");
+                    int b = int.Parse(Console.ReadLine());
+                    Console.WriteLine($"{basic.Ddivide(a, b)}");
+                    break;
+                
+                case 2:
+                    Console.WriteLine("Type Arg1");
+                    int c = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Type Arg2");
+                    int d = int.Parse(Console.ReadLine());
+                    Console.WriteLine($"{basic.Mmultiply(c,d)}");
+                    break;
+                case 3:
+                    Console.WriteLine("Type Arg1");
+                    int e = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Type Arg2");
+                    int f= int.Parse(Console.ReadLine());
+                    Console.WriteLine($"{basic.Aadd(e, f)}");
+                    break;
+                case 4:
+                    Console.WriteLine("Type Arg1");
+                    int g = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Type Arg2");
+                    int h = int.Parse(Console.ReadLine());
+                    Console.WriteLine($"{basic.Ssubtract(g, h)}");
+                    break;
+
+            }
+
+
+
+        }
+    }
+}
