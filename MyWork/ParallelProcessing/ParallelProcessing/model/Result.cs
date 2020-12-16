06cf17e4b71bba2fc5067b036c82018d928815eb
Sabin Bajracharya <sabin.bajracharya@gmail.com>
2020-12-16 18:32:56 +0100

    Add: Model class for CSV record

---
+++ MyWork/ParallelProcessing/ParallelProcessing/model/Result.cs
@@ -0,0 +1,19 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParallelProcessing.model
+{
+    class Result
+    {
+        [Name("Sequential")]
+        public long Sequential { get; set; }
+        
+        [Name("Thread")]
+        public long Thread { get; set; }
+
+        [Name("Parallel")]
+        public long Parallel { get; set; }
+    }
+}
