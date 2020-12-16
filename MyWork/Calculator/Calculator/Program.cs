61b75795080bee75b8746c9927ee72a970075762
Usama Shakoor <usama.shakoor10@gmail.com>
2020-12-17 00:22:31 +0100

    SelfContained Calculator. Single file and trimmed
    SelfContained Calculator. Single file and trimmed

--- MyWork/Calculator/Calculator/Program.cs
+++ MyWork/Calculator/Calculator/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using Calculation;
+using System.Threading.Tasks;
 
 namespace Calculator
 {
     class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World, this is my basic calculator! What operation do you want,choose number? ");
             Console.WriteLine("1 Divide");
@@ -21,6 +22,7 @@ namespace Calculator
                     Console.WriteLine("Type Arg2");
                     int b = int.Parse(Console.ReadLine());
                     Console.WriteLine($"{basic.Ddivide(a, b)}");
+                    await Task.Delay(2000);
                     break;
                 
                 case 2:
@@ -29,6 +31,7 @@ namespace Calculator
                     Console.WriteLine("Type Arg2");
                     int d = int.Parse(Console.ReadLine());
                     Console.WriteLine($"{basic.Mmultiply(c,d)}");
+                    await Task.Delay(2000);
                     break;
                 case 3:
                     Console.WriteLine("Type Arg1");
@@ -36,6 +39,7 @@ namespace Calculator
                     Console.WriteLine("Type Arg2");
                     int f= int.Parse(Console.ReadLine());
                     Console.WriteLine($"{basic.Aadd(e, f)}");
+                    await Task.Delay(2000);
                     break;
                 case 4:
                     Console.WriteLine("Type Arg1");
@@ -43,6 +47,7 @@ namespace Calculator
                     Console.WriteLine("Type Arg2");
                     int h = int.Parse(Console.ReadLine());
                     Console.WriteLine($"{basic.Ssubtract(g, h)}");
+                    await Task.Delay(2000);
                     break;
 
             }
