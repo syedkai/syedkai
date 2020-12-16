0fd28304344dbecdcc94a366e6d90f9fe2483856
Sabin Bajracharya <sabin.bajracharya@gmail.com>
2020-12-16 18:32:10 +0100

    Add: Helper file to write list of data to csv file

---
+++ MyWork/ParallelProcessing/ParallelProcessing/CSV.cs
@@ -0,0 +1,48 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace ParallelProcessing
+{
+    class CSV<T>
+    {
+        private string path;
+
+        public CSV(string path)
+        {
+            this.path = path;
+        }
+
+        public CSV()
+        {
+
+        }
+
+        public void save(List<T> records)
+        {
+            if (path == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            write(path, records);
+        }
+
+        public void save(string path, List<T> records)
+        {
+            write(path, records);
+        }
+
+        private void write(string path, List<T> records)
+        {
+            using (var writer = new StreamWriter(path))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(records);
+            }
+        }
+    }
+}
