d27e19fa2a2f0493438c296ce203e6d263f045de
Al Imran Ahmed <al.imran.cse@gmail.com>
2020-12-16 09:44:22 -0800

    #E7: Reading from configuration file

---
+++ MyWork/E7_ReadingConfigurations/E7_ReadingConfigurations/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using System;
+
+namespace E7_ReadingConfigurations
+{
+    class Program
+    {
+        private static ConfigurationBuilder m_Builder;
+        private static IConfigurationRoot m_Config;
+
+        static void Main(string[] args)
+        {
+            m_Builder = new ConfigurationBuilder();
+            m_Builder.AddJsonFile("appsettings.json");
+            m_Builder.AddCommandLine(args);
+            m_Config = m_Builder.Build();
+            Console.WriteLine(m_Config["Setting1"]);
+        }
+    }
+}
