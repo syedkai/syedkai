712d6607cadd36fab66a56413cd72b24aa1318cf
sweta13 <sweta.swain@stud.fra-uas.de>
2020-12-17 23:05:48 +0100

    Working with Nuget packages

---
+++ MyWork/WorkingWithNuget/WorkingWithNuget/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MyLibrary;
+
+namespace WorkingWithNuget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyLibrary.Mylib refLib = new MyLibrary.Mylib();
+            var res = refLib.calculate(2.2 , 4.4);
+            Console.WriteLine("The result for the caluculation is: "+ res);
+        }
+    }
+}
