c2beabd7561513df17f215ae39e4416e988cfecd
sweta13 <sweta.swain@stud.fra-uas.de>
2020-12-18 00:29:52 +0100

    Unit Testing Calculation Library

---
+++ MyWork/WorkingWithUnitTests/WorkingWithUnitTests/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WorkingWithUnitTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing My work using Unit tests!!");
+            CalculationLibrary.CalculationTypes refLib = new CalculationLibrary.CalculationTypes();
+            var res = refLib.multiply(2.2, 4.4);
+            Console.WriteLine("The result for the caluculation is: " + res);
+        }
+    }
+}
