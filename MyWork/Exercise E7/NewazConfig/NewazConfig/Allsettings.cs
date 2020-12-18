1c1c78bd2315bf6e49bf785d3c48d64b66e5df25
newaz96 <74206051+newaz96@users.noreply.github.com>
2020-12-18 22:43:32 +0600

    E7

---
+++ MyWork/Exercise E7/NewazConfig/NewazConfig/Allsettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace NewazConfig
+{
+    public class Allsettings
+    {
+        public string Setting1 { get; set; }
+
+        public int Setting2 { get; set; }
+
+        public float Setting3 { get; set; }
+
+        public MySettings MySetting { get; set; }
+    }
+}
