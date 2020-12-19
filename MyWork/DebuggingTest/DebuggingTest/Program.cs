0aa6da7e77eaeb6296f6383a83aa45569b2c3456
Usama Shakoor <usama.shakoor10@gmail.com>
2020-12-19 14:07:02 +0100

    Debugger use
    Degugger use

---
+++ MyWork/DebuggingTest/DebuggingTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+namespace DebuggingTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            int i = 0;
+            while (true)
+            {
+                Console.WriteLine($"{i}Hello World");
+                Console.WriteLine($"{i}Hello World, we are debugging");
+
+                Console.WriteLine($"{i}Hello World we are debugging2");
+
+                Console.WriteLine($"{i}Hello World we are debugging3");
+
+                await Task.Delay(1000);
+                i++;
+            }
+        }
+    }
+}
