a6b0c624ecd7edaee80ce3e91b52595865686aca
BIMAL-PC\BIMAL <bimalsah1996@gmail.com>
2020-12-19 23:48:09 +0100

    Updating Exercise 3 and 4

---
+++ MyWork/Exercise4/LCMHCF.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise4
+{
+    class LCMHCF
+    {
+        public static int GCD(int n1, int n2)
+        {
+            int gcf = 1;
+            int i = 1;
+            while (i <= n1 & i <= n2)
+            {
+                if (n1 % i == 0 & n2 % i == 0)
+                {
+                    gcf = i;
+                }
+                i = i + 1;
+            }
+            return gcf;
+        }
+
+        public static int LCM(int n1, int n2)
+        {
+            int lcm = 0;
+            int i = 1;
+            while (n1 >= n2)
+            {
+                int mtl = n1 * i;
+                if (mtl % n2 == 0)
+                {
+                    lcm = mtl;
+                    break;
+                }
+                i = i + 1;
+            }
+            while (n1 < n2)
+            {
+                int mtl = n2 * i;
+                if (mtl % n1 == 0)
+                {
+                    lcm = mtl;
+                    break;
+                }
+                i = i + 1;
+            }
+
+            return lcm;
+        }
+    }
+}
---
+++ MyWork/Exercise4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the numbers");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int gcf = LCMHCF.GCD(n1,n2);
+            int lcm = LCMHCF.LCM(n1, n2);
+            Console.WriteLine("The GCF of the two numbers is "+gcf);
+            Console.WriteLine("The LCM of the two numbers is " + lcm);
+        }
+    }
+}
