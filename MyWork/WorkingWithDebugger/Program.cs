72c5a8cc35baee789e4e65a9df47204fa5ff7337
Abbad Zafar <abbad.zafar@stud.fra-uas.de>
2020-12-20 14:15:29 +0500

    Exercise 6

---
+++ MyWork/WorkingWithDebugger/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace WorkingWithDebugger
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Debugger Test");
+
+            int n = 100;
+
+            for (int i = 0; i <n; i++)
+            {
+
+                Console.WriteLine($"Counter {i}.");
+                await Task.Delay(500);
+            }
+
+            Console.WriteLine(" Press any key to exit");
+            Console.ReadLine();
+
+        }
+    }
+}
