998a38c70b890e0131d46adcbfb34a782c1efd0a
Sudhakar Vundavalli <53111237+Sudhakar3377@users.noreply.github.com>
2020-12-20 19:27:53 +0100

    Code updated

--- MyWork/E7/ConfigSample/ConfigSample/Program.cs
+++ MyWork/E7/ConfigSample/ConfigSample/Program.cs
@@ -1,31 +1,16 @@
-﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Globalization;
 using System.IO;
 
 namespace ConfigSample
 {
-    /// <summary>
-    /// Demonstrates how to read settings
-    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-
-            //var timeUtc = DateTime.UtcNow;
-            //var time = DateTime.Now;
-
-            //var res1 = float.Parse("7,1", CultureInfo.InvariantCulture);
-            //var res2 = float.Parse("7.1", CultureInfo.InvariantCulture);
-
             Console.WriteLine("Reading configuration sample!");
-
             var builder = new ConfigurationBuilder()
-
-           // Fluent API
-
-
            .SetBasePath(Directory.GetCurrentDirectory())
            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
            .AddCommandLine(args)
@@ -44,18 +29,9 @@ namespace ConfigSample
 
             //
             // From root of appsettings.json
-            var setting1 = configuration["Setting1"];
-            var setting2 = configuration["Setting2"];
-            var setting3 = configuration["Setting3"];
-
-            float i = float.Parse(setting3, CultureInfo.InvariantCulture);
-            //
-            // Demonstrates how to read settings from sub section.
-            var section = configuration.GetSection("MySubSettings");
-            var subSetting1 = section["Setting1"];
-            var subSetting2 = section["Setting2"];
-            var subSetting3 = section["Setting3"];
-
+            var Name = configuration["Name"];
+            var Course = configuration["Course"];
+            var CGPA = configuration["CGPA"];
 
             //
             // Reads environment variables
@@ -64,15 +40,20 @@ namespace ConfigSample
             var aaaa = configuration["AAAA"];
             //
             // Demonstrates how to read typed settings
-            MySettings mySettings = new MySettings();
-            configuration.GetSection("MySetting").Bind(mySettings);
+            //MySettings mySettings = new MySettings();
+            //configuration.GetSection("MySetting").Bind(mySettings);
             //Console.ReadLine();
 
             //
             // Demonstrate how to load all settings.
             AllSettings allSettings = new AllSettings();
             configuration.Bind(allSettings);
+
+            Console.WriteLine($"{Name}");
+            Console.WriteLine($"Your Course name is {Course}");
+            Console.WriteLine($"& CGPA is {CGPA}");
             Console.ReadLine();
+            //Console.ReadLine();
         }
 
     }
