4af9112e287d18286325aff3e83c88959750d75b
Usama Shakoor <usama.shakoor10@gmail.com>
2020-12-20 22:22:58 +0100

    E8
    E8

---
+++ MyWork/E8_Interfaces/E8_Interfaces/MedianImpl.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E8_Interfaces
+{
+    class MedianImpl : IMyInterface
+    { private double median;
+        public object GetResult()
+        {
+            return this.median;        }
+
+        public void Train(double[] data)
+        {
+            Array.Sort(data);
+            foreach(double value in data)
+            {
+                this.median+= value;
+            }
+            if (data.Length % 2 == 0)
+            {
+                this.median = (data[(data.Length+1) / 2] + data[((data.Length + 1) / 2)-1]) / 2;
+               
+            }
+            else
+            { this.median = data[(data.Length) / 2]; }
+           
+        }
+    }
+}
