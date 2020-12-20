b39c6abf266a6902efea356b93ba7835d8e8fcd7
Usman Ali <ausman882@gmail.com>
2020-12-20 16:44:54 +0100

    Exercise 7

---
+++ MyWork/Configuration/ConsoleApp1/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class AppSettings
+    {
+        public Smtp Smtp { get; set; }
+        public string ConnectionString { get; set; }
+
+    }
+}
---
+++ MyWork/Configuration/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using ClassLibrary12;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true);
+            IConfigurationRoot config = builder.Build();
+            AppSettings settings = config.Get<AppSettings>();
+            Smtp smtp = settings.Smtp;
+            Console.WriteLine($"Username: {smtp.Username}\nConnectionString: {settings.ConnectionString}");
+            Console.WriteLine("Hello World This is my project!");
+            Person usman = new Person("Usman Ali");
+            Console.WriteLine("Hello I am " + usman.Name);
+        }
+    }
+}
