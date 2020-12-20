2fc92f363ac6bdbfc1f23992b06d5113023e5e78
chiemerie-arum <emerie.arum@yahoo.com>
2020-12-20 16:22:14 +0100

    created and referenced a nuget package

---
+++ MyWork/Nuget-Package/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Nuget_Package
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
