14b2b70cca1e1a7a73cc5a5b620296cfbda03406
Sudhakar3377 <sudhakar.vundavalli@stud.fra-uas.de>
2020-12-21 22:29:12 +0100

    E8 done

---
+++ MyWork/Ex8/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex8
+{
+    public class Class1 : IInterface2
+    {
+        private double m_sum;
+        public Class1()
+        {
+            this.m_sum = 0;
+        }
+        public object GetResult()
+        {
+            return m_sum;
+        }
+
+        public void Train(double[] data)
+        {
+
+            foreach (var number in data)
+            {
+                this.m_sum = m_sum + number;
+            }
+            if (this.m_sum > 100)
+            {
+                this.m_sum = 1;
+            }
+            else
+
+            {
+                this.m_sum = 0;
+            }
+
+
+        }
+    }
+}
---
+++ MyWork/Ex8/Interface2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex8
+{
+    public interface IInterface2
+    {
+        object GetResult();
+        void Train(double[] data);
+    }
+}
