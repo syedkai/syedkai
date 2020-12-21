bc944233cb01287d317bdc00a0a41ae96b660d59
SnigdhaKantam <snigdhakatm@gmail.com>
2020-12-22 03:28:19 +0530

    Nuget packages learning

---
+++ MyWork/AccessingANugetPackage/AccessingANugetPackage/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using CalculatorNugetPackage;
+
+namespace AccessingANugetPackage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to calculator.Please enter two numbers and operation symbol");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            string symbol = Console.ReadLine();
+            Calculate calObj = new Calculate();
+            double result = 0;
+            switch (symbol)
+            {
+                case "+":
+                    result = calObj.Add(a, b);
+                    Console.WriteLine(result);
+                    break;
+                case "-":
+                    result = calObj.Subtract(a, b);
+                    Console.WriteLine(result);
+                    break;
+                case "*":
+                    result = Calculate.Multiply(a, b);
+                    Console.WriteLine(result);
+                    break;
+                case "/":
+                    double res = Calculate.Divide(a, b);
+                    Console.WriteLine(res);
+                    break;
+                default:
+                    Console.WriteLine("Please enter symbols only +,*,/,-");
+                    break;
+            }
+        }
+    }
+}
---
+++ MyWork/CalculatorNugetPackageCreating/CalculatorNugetPackage/Calculate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CalculatorNugetPackage
+{
+    public class Calculate
+    {
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+        public static double Divide(double a, double b)
+        {
+            return (double)a / b;
+        }
+    }
+}
