6e30774f12f7c2b5a5cdb0f8f35fffadebecbcde
KaziMubin <kazimdmubin@gmail.com>
2020-12-22 13:12:39 +0600

    new class added

---
+++ Mywork/Helloworldwithclass/Helloworldwithclass/Helloworldwithclass/Fibonaccigenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helloworldwithclass
+{
+    class Fibonaccigenerator
+    {
+        internal int[] SerieseGenrator(int first_value, int second_value)
+        {
+            int[] fibonacci = new int[20] {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+            return fibonacci;
+        }
+    }
+}
---
+++ Mywork/Helloworldwithclass/Helloworldwithclass/Helloworldwithclass/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Helloworldwithclass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            int input2 = Convert.ToInt32(Console.ReadLine());
+            Fibonaccigenerator serise = new Fibonaccigenerator();
+
+            var fibonacciserise = serise.SerieseGenrator(input1, input2);
+
+            Console.WriteLine(fibonacciserise);
+        }
+    }
+}
