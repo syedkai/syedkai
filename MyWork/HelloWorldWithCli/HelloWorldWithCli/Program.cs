5c3dfc430d262910124a3f5998b437a7eb026ae7
aishincp <aishinahmed20@gmail.com>
2020-12-23 12:03:57 +0100

    Exercise 1 is done

---
+++ MyWork/HelloWorldWithCli/HelloWorldWithCli/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorldWithCli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "World";
+            Console.WriteLine($"Hello {name.ToUpper()}!");
+        }
+    }
+}
