e83401c1430e3f93dc7e592f2fbf8de7a4f6591d
aishincp <aishinahmed20@gmail.com>
2020-12-25 15:44:20 +0100

    Updates

---
+++ MyWork/HelloWorldWithVS/HelloWorldWithVS/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace HelloWorld
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello in .Net World");
+			Console.WriteLine("\nWhat's your name? ");
+			var name = Console.ReadLine();
+			var date = DateTime.Now;
+			Console.WriteLine($"\nHello, {name}, on {date:d}, at {date:t}!");
+			Console.Write("\nPress any key to exit...");
+			Console.ReadKey(true);
+		}
+	}
+}
