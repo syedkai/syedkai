46b3d9753c697492107066dd0d3993427cc29aa2
Syed Kaif <Syedkaif339@gmail.com>
2020-12-26 18:10:06 +0500

    Exercice 4 is done.

---
+++ MyWork/HelloWorldWithClass/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldWithClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Print print_object = new Print();
+            print_object.print_text("Hello World");
+        }
+    }
+}
