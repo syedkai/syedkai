b298b65fd90437f73a17e0cd5d5f10e1116e5c2f
Ishfaq Ahmad Jadoon <ashfaq.khan2235@gmail.com>
2020-12-28 02:02:16 +0100

    Exercise 10

---
+++ MyWork/Ex 10/Ex 10/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
