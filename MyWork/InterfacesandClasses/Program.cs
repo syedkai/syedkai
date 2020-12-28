312a1a1c881ac20207c22643d6d8554fa65f7f12
Abbad Zafar <abbad.zafar@stud.fra-uas.de>
2020-12-28 20:23:43 +0500

    Exercise 8

---
+++ MyWork/InterfacesandClasses/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace InterfacesandClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            Sample1();
+
+            PersonInterface Per1 = new Person1();
+            Per1.PrintName("Abbad");
+
+            PersonInterface Per2 = new Person2();
+            Per2.PrintName("Zafar");
+
+
+
+        }
+
+
+        /// <summary>
+        /// Demonstrates how to implement and use two classes, that implement some math algorithms.
+        /// </summary>
+        private static void Sample1()
+            {
+                int[] data = new int[] { 1, 22, 42, 11, 44 };
+
+                SumAlgorithm sum = new SumAlgorithm();
+
+                sum.Train(data);
+
+                var sumRes = sum.GetResult();
+
+                Console.WriteLine(sumRes);
+
+
+                AverageAlgorithm avg = new AverageAlgorithm();
+
+                avg.Train(data);
+
+                var averageRes = avg.GetResult();
+
+                Console.WriteLine(averageRes);
+            }
+
+
+         
+        }
+
+    
+}
+
