0be61357cf3e68af00c09638054079d8ee11279c
KaziMubin <kazimdmubin@gmail.com>
2020-12-28 22:02:10 +0600

    Exercise 6 Framework Independant Build

---
+++ Mywork/FramworkIndependant/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FramworkIndependant
+{
+    class Class1
+    {
+        public int Addition(int[] input)
+        {
+            return input[0] + input[1];
+        }
+
+        public int Subtraction(int[] input)
+        {
+            return input[0] - input[1];
+        }
+        public int Multiplication(int[] input)
+        {
+            return input[0] * input[1];
+        }
+        public int Divition(int[] input)
+        {
+            return input[0] / input[1];
+        }
+    }
+}
---
+++ Mywork/FramworkIndependant/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace FramworkIndependant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome in your calculator!!!");
+
+            // getting input method
+            int[] input1 = gettingInput();
+
+            // action to do
+            Console.WriteLine("Give an operation +,-,/,*");
+            string action = Console.ReadLine();
+
+            int output;
+            Class1 cal = new Class1();
+            // calculating output
+            switch (action)
+            {
+                case "+":
+                    output = cal.Addition(input1);
+                    Console.WriteLine($"Summation of this two values is {output}");
+                    break;
+                case "-":
+                    output = cal.Subtraction(input1);
+                    Console.WriteLine($"Summation of this two values is {output}");
+                    break;
+                case "*":
+                    output = cal.Multiplication(input1);
+                    Console.WriteLine($"Summation of this two values is {output}");
+                    break;
+                case "/":
+                    output = cal.Divition(input1);
+                    Console.WriteLine($"Summation of this two values is {output}");
+                    break;
+            }
+        }
+
+        private static int[] gettingInput()
+        {
+            int[] inputValue = { 0, 0 };
+            inputValue[0] = Convert.ToInt32(Console.ReadLine());
+            inputValue[1] = Convert.ToInt32(Console.ReadLine());
+
+            return inputValue;
+        }
+
+    }
+}
+
+
+
