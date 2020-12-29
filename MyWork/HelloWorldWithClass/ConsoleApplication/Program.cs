0f8e9594be79b7591bd757995e67a13c9450b982
aishincp <aishinahmed20@gmail.com>
2020-12-29 16:40:23 +0100

    Exercise 4

---
+++ MyWork/HelloWorldWithClass/ConsoleApplication/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            stringUtility.StringLibrary clsString = new stringUtility.StringLibrary();
+            Console.WriteLine(clsString.StartsWithUpper("Sts"));
+        }
+    }
+}
