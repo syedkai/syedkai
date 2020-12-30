40fdced1582f0a78dddc89f333d37d6fca37c63b
Usmanali3599 <usman882@gmail.com>
2020-12-30 23:55:49 +0100

    Exercise 10 Working with NuGet
    Created a NuGet from custom library and installed it to my project from a lcoal source

---
+++ MyWork/E10/E10/Program.cs
@@ -0,0 +1,15 @@
+﻿using MyLib;
+using System;
+
+namespace E10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Person person = new Person("Usman", "Ali");
+            Console.WriteLine(person.FullName);
+        }
+    }
+}
---
+++ MyWork/E10/MyLib/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MyLib
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                string fullName = "";
+                if (!string.IsNullOrWhiteSpace(FirstName))
+                {
+                    fullName += FirstName;
+                }
+                if (!string.IsNullOrWhiteSpace(LastName))
+                {
+                    if (!string.IsNullOrWhiteSpace(fullName))
+                        fullName = $"{fullName} {LastName}";
+                    else
+                        fullName += LastName;
+                }
+                return fullName;
+            }
+        }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+    }
+}
