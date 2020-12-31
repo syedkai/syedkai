2791195b60be5cd078dd6ff8a3b8b263249cdfe9
SyedJaffar1 <74202369+SyedJaffar1@users.noreply.github.com>
2020-12-31 20:12:23 +0500

    Update

---
+++ MyWork/HelloWorldWithClass/HelloWorldWithClass/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HelloWorldWithClass
+{
+    public class Person
+    {
+
+        public string Name;
+        public int age;
+        public bool Haspet;
+
+        public void Greeting()
+        {
+            Console.WriteLine("HelloWorld");
+            Console.WriteLine("My name is" + Name + " and my age is " + age);
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = " Jaffar";
+            person.age = 26;
+            person.Haspet = true;
+            person.Greeting();
+
+        }
+    }
+}
