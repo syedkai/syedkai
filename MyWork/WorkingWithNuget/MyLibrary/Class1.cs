f19999132d165b28b967bd09f11bdbfe34d03a5d
Abbad Zafar <abbad.zafar@stud.fra-uas.de>
2020-12-31 11:37:29 +0500

    Excecise 10

---
+++ MyWork/WorkingWithNuget/MyLibrary/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyLibrary
+{
+    public class MyLib
+    {
+        public int calculate(int a, int b)
+        {
+
+            return a + b;
+        }
+    }
+}
---
+++ MyWork/WorkingWithNuget/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WorkingWithNuget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            MyLibrary.MyLib referencedLib = new MyLibrary.MyLib();
+            var result = referencedLib.calculate(12, 12);
+            Console.WriteLine(result);
+        }
+    }
+}
