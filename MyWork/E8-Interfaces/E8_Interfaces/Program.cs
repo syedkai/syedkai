cb43f2ef2289b3d9c2d6a7324785aef892738090
SyedJaffar1 <74202369+SyedJaffar1@users.noreply.github.com>
2021-01-01 00:52:13 +0500

    E8-Interfaces

---
+++ MyWork/E8-Interfaces/E8_Interfaces/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace E8_Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Double[] data = new double[] { 2, 9, 1, 50, 10, 3,7,8 };
+            Array.Sort(data);
+            foreach (int i in data) { 
+                Console.Write(i + " ");
+            }
+            IMyInterface sum = new SumImpl();
+            IMyInterface median = new MedianImpl();
+            Binary(sum,data);
+            Median(median,data);
+
+        }
+
+        private static void Median(IMyInterface median,double[] data)
+        {
+            
+            median.Train(data);
+            Console.WriteLine($"Median ={median.GetResult()}");
+        }
+
+        private static void Binary(IMyInterface sum,double[] data)
+        {
+            
+            sum.Train(data);
+            Console.WriteLine($"Binary ={sum.GetResult()}");
+        }
+    }
+}
