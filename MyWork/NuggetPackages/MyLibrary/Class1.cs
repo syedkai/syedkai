d30992e3c7f680a1cc78179b414bea1d5e0cee1c
Janet007 <root@Janets-MacBook-Pro.local>
2020-12-31 23:52:27 +0100

    E10 Working With NuGet

---
+++ MyWork/NuggetPackages/MyLibrary/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyLibrary
+{
+    public class Class1
+    {
+        public int Calculate(int a, int b)
+
+        {
+            return a + b;
+        }
+    }
+}
---
+++ MyWork/NuggetPackages/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Workingwithnugget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            MyLibrary.Class1 referencedLib = new MyLibrary.Class1();
+
+            var res = referencedLib.Calculate(23, 13);
+
+            Console.WriteLine(res);
+
+        }
+    }
+}
