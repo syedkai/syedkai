bc2e01523a19f25b3bf0e342dafa491aa9054b98
SyedJaffar1 <74202369+SyedJaffar1@users.noreply.github.com>
2020-12-31 20:05:22 +0500

    update

---
+++ MyWork/Working with debugger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HelloWorldWithClass
+{
+    public class Person
+    {
+
+        public string Name;
+        public int age;
+        public bool Haspet;
+
+        public void Greeting()
+        {
+            Console.WriteLine("My name is" + Name + " and my age is " + age);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = " Jaffar";
+            person.age = 26;
+            person.Haspet = true;
+            person.Greeting();
+
+        }
+    }
+}
