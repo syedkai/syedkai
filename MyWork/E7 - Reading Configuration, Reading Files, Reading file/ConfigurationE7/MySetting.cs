a5e920ff450ec6e0be4cf053a22954511788c74f
SyedJaffar1 <74202369+SyedJaffar1@users.noreply.github.com>
2021-01-01 00:48:16 +0500

    exercise 7

---
+++ MyWork/E7 - Reading Configuration, Reading Files, Reading file/ConfigurationE7/MySetting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigurationE7
+{
+    public class MySetting
+    {
+        public int Port { get; set; }
+        public string UserApplication { get; set; }
+        public string Socket { get; set; }
+    }
+}
---
+++ MyWork/E7 - Reading Configuration, Reading Files, Reading file/ConfigurationE7/MyySetting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigurationE7
+{
+    public class MyySetting
+    {
+        public MySetting MySettings { get; set; }
+        public string IP1 { get; set; }
+        public string IP2 { get; set; }
+        public string IP3 { get; set; }
+    }
+}
