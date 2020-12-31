2b1b3094ee2254fadf76a8b40c5d62cac07b9c75
Usama Shakoor <usama.shakoor10@gmail.com>
2020-12-31 13:44:12 +0100

    Task10
    NugetPakages

---
+++ MyWork/Task10/Task10/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Calculation.basic calculate = new Calculation.basic();
+            var result=calculate.Ddivide(2,3);
+            Console.WriteLine(result);
+        }
+    }
+}
