c54421cd2e06e7d43a0e7efba87050ad2ccf80db
Nicodemus-Onah <74201192+Nicodemus-Onah@users.noreply.github.com>
2020-12-31 21:58:34 +0100

    2nd and 3rd task

---
+++ MyWork/ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+
+}
