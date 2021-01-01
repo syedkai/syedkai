7a0dae605a663e60bf1c2d14babe8335b2a6d151
VDPurohit <purohitvivek7@gmail.com>
2021-01-01 16:52:23 +0100

    Ex9 almost done but only start own job thread remaining

--- MyWork/HelloWorldCli/Ex8/Ex8/Program.cs
+++ MyWork/HelloWorldCli/Ex8/Ex8/Program.cs
@@ -8,10 +8,12 @@ namespace Ex8
         {
             IMyInterface sumInterface = new Sum();
             Console.ReadLine();
-            Console.WriteLine("Result of Sum : {0}", sumInterface.Train(10,22));
+            Console.WriteLine("Result of Sum : {0}", sumInterface.Train(10, 22));
 
             IMyInterface avgInterface = new Average();
             Console.WriteLine("Result of Average : {0}", avgInterface.Train(10, 22));
+            Console.WriteLine("Press Enter to exit");
+            Console.ReadLine();
         }
     }
 }
---
+++ MyWork/HelloWorldCli/Ex9/Ex9/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace Ex9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadHandling t = new ThreadHandling();
+            t.mutlithreadingExicution();
+            t.StartJobInSeqence();
+        }
+    }
+}
+
+class ThreadHandling
+{
+    Action<Action> measure = (body) => {
+        var startTime = DateTime.Now;
+        body();
+        Console.WriteLine("{0} {1}", DateTime.Now - startTime, Thread.CurrentThread.ManagedThreadId);
+    };
+
+    int finishCounter = 0;
+
+    public void mutlithreadingExicution()
+    {
+        Action CalcJob = () => { for (int i = 0; i < 3500000; i++) ; };
+        Action ioJob = () => { Thread.Sleep(1000); };
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Multi Threading");
+        measure(() =>
+        {
+
+            var tasks = Enumerable.Range(1, 10)
+            .Select(_ => Task.Factory.StartNew(() => measure(ioJob)))
+            .ToArray();
+            Task.WaitAll(tasks);
+            Console.ReadLine();
+        });
+    }
+
+
+    public void StartJobInSeqence()
+    {
+        Console.WriteLine("Start Job In Seqence");
+        try
+        {
+            Thread.CurrentThread.Name = "Single Main Thread";
+        }
+        catch { }
+        finally
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                measure(() =>
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Job Sequence No: {0}", i);
+                });
+            }
+
+        }
+
+    }
+}
