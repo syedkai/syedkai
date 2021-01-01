682a3053e1516be246679c8655e3263663f08b6b
junaid Khalid <junaidkhalid185@gmail.com>
2021-01-02 03:42:01 +0500

    E10 - Working with NuGet

---
+++ MyWork/WorkingWithNuGet/WorkingWithNuGet/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Bank;                         // Using my Package
+
+namespace WorkingWithNuGet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100;
+            BankAccount b = new BankAccount(a);
+            Console.WriteLine("Your Bank Balance is {0}",a);
+
+            Console.WriteLine("Please Enter the ammount to withdraw: ");
+            int c = int.Parse(Console.ReadLine());
+            b.WithdrawAmt(c);
+
+
+
+
+        }
+    }
+}
