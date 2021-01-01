9f2e6d0fb78f636d8e37a1a7e4b7a08cbd1ea507
VDPurohit <purohitvivek7@gmail.com>
2021-01-01 17:17:34 +0100

    Ex 10 is done upload a library on Nuget

---
+++ MyWork/HelloWorldCli/Ex10/Ex10/Calculation/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex10.Calculation
+{
+    public class Calculator
+    {
+        public static double Add(double firstArgs, double secondArgs) {
+            return firstArgs + secondArgs;
+        }
+
+        public static double Substract(double firstArgs, double secondArgs)
+        {
+            return firstArgs - secondArgs;
+        }
+    }
+}
