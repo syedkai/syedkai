1f608c007644110e209aa2e6899f53fdf202166d
daian <pirmoradianm381@yahoo.com>
2021-01-01 11:47:58 +0100

    Hello With Cli

---
+++ MyWork/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
