0d4878f85c5d5740fda383b7e48d826d7f4110b4
chiemerie-arum <emerie.arum@yahoo.com>
2021-01-01 15:55:24 +0100

    Exercise 9, Parallel Processing

---
+++ MyWork/Parallel-Processing/ParallelProcessingNetCoreV3.1/ThreadingNetCore3.1/Parallel.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadingSampleNetCorev3._1
+{
+    class Parallel
+    {
+
+        private long m_FinishCounter = 0;
+
+        public void StartEachJobInOwnThread(int threads, Action<object> func)
+        {
+            for (int i = 0; i < threads; i++)
+            {
+                Thread t = null;
+                try
+                {
+                    t = new Thread(new ParameterizedThreadStart(func));
+                    t.Name = i.ToString();
+                    t.Start(new Action<string>(OnThreadFinished));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            while (true)
+            {
+                if (Interlocked.Read(ref m_FinishCounter) == threads)
+                    break;
+                else
+                    Thread.Sleep(500);
+            }
+        }
+
+        public void StartJobInSeqence(int sequences, Action<object> func)
+        {
+            try
+            {
+                Thread.CurrentThread.Name = "Single Thread";
+                for (int i = 0; i < sequences; i++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Sequence No: {0}", i);
+                    func(null);
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private void OnThreadFinished(string threadName)
+        {
+            Interlocked.Increment(ref m_FinishCounter);
+        }
+
+
+
+        public void StartJobsInParallelTask(int threads, Action<object> func)
+        {
+
+
+            List<Task> tList = new List<Task>();
+
+            for (int i = 0; i < threads; i++)
+            {
+                Task t = null;
+                try
+                {
+                    t = new Task(func, new Action<string>(OnThreadFinished));
+                    tList.Add(t);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            foreach (var t in tList)
+            {
+                t.Start();
+            }
+
+            Task.WaitAll(tList.ToArray());
+
+        }
+
+    }
+}
---
+++ MyWork/Parallel-Processing/ParallelProcessingNetCoreV5.0/ThreadingNetCore5.0/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace ThreadingSampleNetCorev3._1
+{
+    class Program
+    {
+        private static readonly int numberOfThreads = 10;
+        private static readonly int numberOfProcessors = 8;
+
+        static void Main(string[] args)
+        {
+            Process.GetCurrentProcess().ProcessorAffinity = (System.IntPtr)numberOfProcessors;
+            Parallel parallel = new Parallel();
+            Stopwatch stopWatch = new Stopwatch();
+
+            /*
+             * Execution in Sequence
+            */
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Job in Own Thread");
+            stopWatch.Start();
+            parallel.StartEachJobInOwnThread(10, Workerfuntion);
+            stopWatch.Stop();
+            Console.WriteLine();
+            Console.WriteLine("Type: Job in Own Thread, " + " Number of Processors: " + numberOfProcessors + "; Total Threads: " + numberOfThreads + "; Execution time: " + stopWatch.ElapsedMilliseconds + "ms");
+
+            /*
+             * Execution in Sequence
+            */
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Job in Sequence");
+            stopWatch.Start();
+            parallel.StartJobInSeqence(10, Workerfuntion);
+            stopWatch.Stop();
+            Console.WriteLine();
+            Console.WriteLine("Type: Job in Sequence, " + " Number of Processors: " + numberOfProcessors + "; Total Threads: " + numberOfThreads + "; Execution time: " + stopWatch.ElapsedMilliseconds + "ms");
+
+            /*
+             * Execution in Parallel
+            */
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Job in Parallel");
+            stopWatch.Restart();
+            parallel.StartJobsInParallelTask(10, Workerfuntion);
+            stopWatch.Stop();
+            Console.WriteLine();
+            Console.WriteLine("Type: Job in Parallel, " + " Number of Processors: " + numberOfProcessors + "; Total Threads: " + numberOfThreads + "; Execution time: " + stopWatch.ElapsedMilliseconds + "ms");
+        }
+
+        private static void Workerfuntion(Object onFinishDelegate)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Started thread: {Thread.CurrentThread.Name} - {Thread.CurrentThread.ManagedThreadId}");
+            int r = 1;
+
+            for (int i = 0; i < 3; i++)
+            {
+                r = r + 1;
+            }
+
+            if (onFinishDelegate != null)
+            {
+                ((Action<string>)onFinishDelegate)(Thread.CurrentThread.Name);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Stopped thread: {0}", Thread.CurrentThread.Name);
+
+        }
+    }
+}
