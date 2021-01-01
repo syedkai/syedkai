b92daf682d927671e0e7914fc0327a2f89b46fe5
VDPurohit <purohitvivek7@gmail.com>
2021-01-01 10:27:04 +0100

    Ex7 is done

---
+++ MyWork/HelloWorldCli/EX7/EX7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace EX7
+{
+    class Program
+    {
+        private static ConfigurationBuilder builder;
+        private static IConfigurationRoot config;
+
+        static void Main(string[] args)
+        {
+            ConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("appsettings.json");
+            IConfiguration config = builder.Build();
+            String Num = config["Setting1"];
+            Console.WriteLine(config["Setting1"]);
+            Console.ReadLine();
+        }
+    }
+}
