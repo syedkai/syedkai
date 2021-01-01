f7436c29811164904f896bb3640c95b76437f5dc
VDPurohit <purohitvivek7@gmail.com>
2021-01-01 18:56:20 +0100

    Ex11 is done

---
+++ MyWork/HelloWorldCli/Ex11/Ex11/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Ex11
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Calculator
+    {
+
+        public int Add(int firstArgs, int secondArgs)
+        {
+            return firstArgs + secondArgs;
+        }
+
+        public int Substract(int firstArgs, int secondArgs)
+        {
+            return firstArgs - secondArgs;
+        }
+
+        public int Mutliply(int firstArgs, int secondArgs)
+        {
+            return firstArgs * secondArgs;
+        }
+
+        public double Devide(double firstArgs, double secondArgs)
+        {
+            return firstArgs / secondArgs;
+        }
+
+        public int Square(int args)
+        {
+            return args * args;
+        }
+    }
+}
---
+++ MyWork/HelloWorldCli/Ex11/Ex11Tests/CalculatorTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ex11;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex11.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void CalcTest()
+        {
+            Calculator calc = new Calculator();
+
+            Assert.AreEqual(5, calc.Add(2,3));
+            Assert.AreEqual(7, calc.Substract(10,3));
+            Assert.AreEqual(3, calc.Mutliply(1,3));
+            Assert.AreEqual(2, calc.Devide(4,2));
+            Assert.AreEqual(4,calc.Square(2));
+        }
+    }
+}
