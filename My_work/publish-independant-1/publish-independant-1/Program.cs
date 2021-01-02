1785d58b9240b619c2252571b17ee4f86dcbc8ef
Mandar Hate <mandarhate6897@outlook.com>
2021-01-02 12:49:28 +0100

    Published independantly

---
+++ My_work/publish-independant-1/publish-independant-1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace publish_independant_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Publishing independantly");
+            Console.ReadLine();
+        }
+    }
+}
