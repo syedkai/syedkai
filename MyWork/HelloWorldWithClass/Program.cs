2dc5d10a1bcaa5a246a6e97abc804d6d234621db
Md.Mukit Khan <mukitkhan07@gmail.com>
2021-01-02 18:46:14 +0600

    console app with a reference class

---
+++ MyWork/HelloWorldWithClass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldWithClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
