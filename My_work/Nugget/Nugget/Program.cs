48218ddf140ae772851894305eb96233a155347d
Mandar Hate <mandarhate6897@outlook.com>
2021-01-02 22:20:06 +0100

    Added nugget packages and code for reading settings

---
+++ My_work/Nugget/Nugget/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.CommandLine;
+using Microsoft.Extensions.Configuration.EnvironmentVariables;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace Nugget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+           .SetBasePath(Directory.GetCurrentDirectory())
+           .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+           .AddCommandLine(args)
+           .AddEnvironmentVariables();
+
+
+            IConfigurationRoot configuration = builder.Build();
+
+
+            var guitar = configuration["guitar"];
+            var phone = configuration["phone"];
+
+
+
+            Console.WriteLine("{0}", guitar);
+            Console.WriteLine("{0}", phone);
+
+        }
+    }
+}
