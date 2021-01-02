f7d67df51634b90913ac13f8bb016c7cf6a16be0
Janet007 <root@Janets-MacBook-Pro.local>
2021-01-02 22:53:26 +0100

    E10exerciseadded

---
+++ MyWork/NuggetPackagesExercise10/MyLibrary/MyLib.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyLibrary
+{
+    public class MyLib
+    {
+
+        public int Calculate(int a, int b)
+        {
+            return a + b;
+        }
+        public double divide(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
+
---
+++ MyWork/NuggetPackagesExercise10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ExerciseE10
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MyLibrary.MyLib referencedLib = new MyLibrary.MyLib();
+            var res = referencedLib.Calculate(11, 23);
+
+            Console.WriteLine(res);
+            Console.WriteLine(referencedLib.divide(10, 2));
+        }
+
+
+    }
+}
