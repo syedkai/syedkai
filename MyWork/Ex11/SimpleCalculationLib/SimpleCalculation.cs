d689e8c5a1d3484a383cc0d144ec5d9ea24e165d
Md.Mukit Khan <mukitkhan07@gmail.com>
2021-01-02 17:41:29 +0600

    Ex 11

---
+++ MyWork/Ex11/SimpleCalculationLib/SimpleCalculation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SimpleCalculationLib
+{
+    public class SimpleCalculation
+    {
+
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Divide(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
