1cfcb05b6a5217edc3c5c22da5e19a7678e5f40c
Mandar Hate <mandarhate6897@outlook.com>
2021-01-02 12:26:03 +0100

    Library created and used ion a console application

---
+++ My_work/Library_consumed/Library_consumed/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace Library_consumed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj = new Class1();
+          double Result =  obj.addition(2.3, 7);
+            Console.WriteLine(Result);
+            Console.ReadLine();
+
+        }
+    }
+}
