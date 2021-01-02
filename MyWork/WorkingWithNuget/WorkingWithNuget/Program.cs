b01f0d541efc94fb72f42a95dcaaad7da4215f5d
Kanav555 <kanav.sharma@stud.fra-uas.de>
2021-01-02 16:32:35 +0100

    Working With Nuget

---
+++ MyWork/WorkingWithNuget/WorkingWithNuget/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WorkingWithNuget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Working With Nuget Packages");
+            MyLibrary.MyLib referencedlib = new MyLibrary.MyLib();
+
+            var result= referencedlib.calc(24, 2);
+            Console.WriteLine("Division Result: "+result);
+
+
+        }
+    }
+}
