dd4b5152c5dc1a25a7ea023750015a1dc04686fd
Mandar Hate <mandarhate6897@outlook.com>
2021-01-02 23:05:23 +0100

    Working with Nuget

---
+++ My_work/Lib_Nug/Lib_Nug/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NugetLib;
+
+namespace Lib_Nug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Multiplication of two numbers is:");
+            double Result = Class1.test1(65, 19);
+
+            Console.WriteLine(Result); ;
+        }
+    }
+}
---
+++ My_work/NugetLib/NugetLib/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NugetLib
+{
+    public class Class1
+    {
+        public static double test1(double x, double y)
+
+        {
+            return x * y;
+        }
+
+    }
+
+}
