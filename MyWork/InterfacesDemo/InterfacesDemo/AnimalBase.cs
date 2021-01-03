8621ccff77e1cac933b3b43b7dbc335fbcdbb080
Phan Bao Viet Nguyen <51410568+vitkocapcap@users.noreply.github.com>
2021-01-03 23:59:23 +0100

    Add files via upload

---
+++ MyWork/InterfacesDemo/InterfacesDemo/AnimalBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfacesDemo
+{
+    internal abstract class AnimalBase
+    {
+        public int Age { get; set; }
+
+        public string Name { get; set; }
+
+        public AnimalBase():this(1)
+        {
+
+        }
+
+        public AnimalBase(string name) : this(1,name)
+        {
+            this.Name = name;
+        }
+
+        public AnimalBase(int age) : this(age, "Anna")
+        {
+            this.Age = age;
+        }
+
+        public AnimalBase(int age, string name)
+        {
+            this.Age = age;
+            this.Name = name;
+        }
+
+    }
+}
---
+++ MyWork/InterfacesDemo/InterfacesDemo/Cat.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfacesDemo
+{
+    internal class Cat : AnimalBase
+    {
+        internal Cat() : base(1)
+        {
+
+        }
+
+        internal Cat(int age) : base(age, "Anna")
+        {
+
+        }
+
+        internal Cat(string name) : base(1, name)
+        {
+
+        }
+
+        internal Cat(int age, string name) : base(age,name)
+        {
+
+        }
+
+        internal void PrintName()
+        {
+            Console.WriteLine(this.Name);
+        }
+
+    }
+}
