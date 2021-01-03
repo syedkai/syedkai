d10bfe6ab494200cc827a86c03c04206d945adbf
Jayshri Taywade <taywadejayshri13@gmail.com>
2021-01-03 18:51:23 +0100

    HelloWorldwithClass added

---
+++ MyWork/HelloWorldWithClass/program.cs
@@ -0,0 +1,21 @@
+using System; 
+  
+
+namespace HelloWorlwithClass
+{ 
+      
+    
+    class Program 
+    { 
+          
+        
+        static void Main(string[] args)
+        { 
+                
+            Console.WriteLine("Hello World!"); 
+              
+            
+            Console.ReadKey(); 
+        } 
+    } 
+} 
