edebdaa91bafded2508559f03d71b765c9ab9d15
chhavi <chhavi@DESKTOP-H8TL7KQ>
2021-01-03 01:12:00 +0100

    publish Independent

---
+++ MyWork/Lesson3/Lesson3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Lesson3
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello lesson3!Deep dive in .Net 5.0");
+
+            int n = 100;
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Counter {n}.");
+                await Task.Delay(1000).ContinueWith((t) =>
+                {
+                    Console.WriteLine("Done with Delay!!");
+                });
+                Console.WriteLine("Press any key to exit");
+                Console.ReadLine();
+
+
+
+            }
+        }
+    }
+}
