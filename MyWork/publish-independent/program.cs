2fb983bcd28c864fb94723d4ad62a3edf8be02ed
Jayshri Taywade <taywadejayshri13@gmail.com>
2021-01-03 19:08:20 +0100

    publish-independent program

---
+++ MyWork/publish-independent/program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PublishIndependant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
