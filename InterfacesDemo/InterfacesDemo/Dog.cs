6f1a1659e086495057dc110d0d308551a6814fe7
Phan Bao Viet Nguyen <51410568+vitkocapcap@users.noreply.github.com>
2021-01-03 23:57:17 +0100

    Add files via upload

---
+++ InterfacesDemo/InterfacesDemo/Dog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfacesDemo
+{
+    internal class Dog : AnimalBase
+    {
+        internal Dog() : base(1)
+        {
+
+        }
+
+        internal Dog(int age) : base(age, "Anna")
+        {
+
+        }
+
+        internal Dog(string name) : base(1, name)
+        {
+
+        }
+
+        internal Dog(int age, string name) : base(age, name)
+        {
+
+        }
+        
+        internal void PrintName()
+        {
+            Console.WriteLine(this.Name);
+        }
+
+
+
+    }
+}
---
+++ InterfacesDemo/InterfacesDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace InterfacesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var c = new Cat();
+            var d = new Dog();
+
+            c.PrintName();
+            d.PrintName();
+            incrementAnimalAge(c);
+            incrementAnimalAge(d);
+        }
+
+        private static void incrementAnimalAge(Dog dog)
+        {
+            dog.Age++;
+        }
+
+        private static void incrementAnimalAge(AnimalBase animal)
+        {
+            animal.Age++;
+        }
+    }
+}
