4d753076ae36bfccc8c0f597aaecdd168d1e3297
Saloni <salgoyal1503@gmail.com>
2021-01-05 22:12:07 +0100

    Ex 4 commited

---
+++ MyWork/ClassProjE4/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassProjE4
+{
+    class Class1
+    {
+
+        string city;
+        string state;
+        string country;
+
+        public Class1(string a, string b, string c)
+        {
+            this.city = a;
+            this.state= b;
+            this.country = c;
+
+        }
+
+        public string getCityName()
+        {
+            return city;
+        }
+
+        public string getState()
+        {
+            return state;
+        }
+
+        public string getCountry()
+        {
+            return country;
+        }
+
+        public string Detail()
+        {
+
+            return ("City :  " + this.getCityName() + " State : " + this.getState() + " Country :  " + this.getCountry());
+
+
+        }
+
+    }
+}
+    
---
+++ MyWork/ClassProjE4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ClassProjE4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 newCity = new Class1("Frankfurt", "Hesse", "Germany");
+            Console.WriteLine(newCity.Detail());
+        }
+    }
+}
