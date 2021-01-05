310e217cd6b89a783b072adeffacf322afaee78a
chhavi <chhavi@DESKTOP-H8TL7KQ>
2021-01-05 12:19:50 +0100

    Ex5

---
+++ MyWork/publish-independent/publish-independent/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+
+namespace publish_independent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            for (int i = 0; i < 100; i++)
+            {
+                double result = (i);
+                Console.WriteLine($"Hello students from Windows! Iteration :{i} generate the result ={result}");
+                Thread.Sleep(500);
+            }
+            Console.WriteLine("Press any key do exit from ...");
+
+            Console.ReadLine();
+        }
+    }
+}
