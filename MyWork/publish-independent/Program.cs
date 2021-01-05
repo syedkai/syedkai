b793b8d5539d7654f5941933e8fc76a10d0ea594
Saloni <salgoyal1503@gmail.com>
2021-01-05 22:17:02 +0100

    Ex 5- publish independent

---
+++ MyWork/publish-independent/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace publish_independent
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Deep dive in NET 5.0");
+
+            int a = 20;
+
+            for (int i = 0; i < a; i++)
+            {
+                Console.WriteLine($"Counter: {i}");
+
+                await Task.Delay(500);
+            }
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+        }
+    }
+}
