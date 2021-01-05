d877fa911735a0502d68016d01c8be23890540c5
chhavi <chhavi@DESKTOP-H8TL7KQ>
2021-01-05 11:20:52 +0100

    Ex3

---
+++ MyWork/HelloWorldwithVs/HelloWorldwithVs/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloWorldwithVs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string SecretWord = "Giraffe";
+            string guess = "";
+            while(guess!= SecretWord)
+            {
+                Console.Write("Enter Guess:");
+                    guess = Console.ReadLine();
+            }
+
+            Console.Write("You Win!");
+            Console.ReadLine();
+        }
+    }
+}
