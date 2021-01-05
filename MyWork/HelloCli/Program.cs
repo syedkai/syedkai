608cc642d2ea4b9d99c76b506fcf06cd6501923e
Saloni <salgoyal1503@gmail.com>
2021-01-05 22:09:13 +0100

    Ex 2

---
+++ MyWork/HelloCli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloCli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
