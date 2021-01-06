28de237f36066c6fb9c5ff3d035c274139ab5ee4
Saloni <salgoyal1503@gmail.com>
2021-01-06 01:09:57 +0100

    Ex 10 - Working with Nuget

---
+++ MyWork/MyLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyLibrary
+{
+    public class Mylib
+    {
+        public double calculate(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ MyWork/WorkingwithNuget/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+//using MyLibrary;
+
+
+namespace WorkingWithNuget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Working With Nuget Packages");
+            MyLibrary.Mylib referencedlib = new MyLibrary.Mylib();
+
+            var result = referencedlib.calculate(24, 2);
+            Console.WriteLine("Division Result: " + result);
+
+
+        }
+    }
+}
