de7ddce5b7c15d4f4dd854f206838f24bfc272d1
Md Rabiul Islam <31253296+rabiul64@users.noreply.github.com>
2021-01-07 21:32:41 +0600

    add Exercise 10 Working with NuGet Package to branch

---
+++ MyWork/HelloWorldWIthNuGet/HelloWorldWIthNuGet/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using TestLibrary;
+
+namespace HelloWorldWIthNuGet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            double[] data = new double[] { 1.4, 2.5, 0.12, 5.5, 3.6 };
+
+            Console.Write("Given the data : ");
+            foreach (var item in data)
+            {
+                Console.Write(item);
+                Console.Write(" ");
+            }
+            Console.WriteLine("");
+
+            var alg = new MyAlgorithm();
+
+            Console.WriteLine($"Sum of the data is : {alg.CalculateSum(data)}");
+            Console.WriteLine($"Average value of the data is : {alg.CalculateAverage(data)}");
+
+            Console.WriteLine("Press any key to end session.");
+            Console.ReadLine();
+        }
+    }
+}
