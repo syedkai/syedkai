dc4310f400751fa2dea81a1afeb202f0284191f6
Al Imran Ahmed <al.imran.cse@gmail.com>
2021-01-07 14:56:39 +0600

    Project initiated

---
+++ MyProject/ImageClassificationWithSpatialPooler/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ImageClassificationWithSpatialPooler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Image classification");
+
+            // Setup Collect the images to classify
+            // Encode all images to SDR
+            // Use special pooling on those SDR
+            // Compare the images
+        }
+    }
+}
