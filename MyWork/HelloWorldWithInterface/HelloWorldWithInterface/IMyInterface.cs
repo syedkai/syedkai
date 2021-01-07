315aa9c54e4088edc77ce82458322d57168f7109
Md Rabiul Islam <31253296+rabiul64@users.noreply.github.com>
2021-01-07 23:04:52 +0600

    add Exercise 8 HelloWorldWithInterface to branch

---
+++ MyWork/HelloWorldWithInterface/HelloWorldWithInterface/IMyInterface.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+
+namespace HelloWorldWithInterface
+{
+    /// <summary>
+    /// interface class
+    /// </summary>
+    interface IMyInterface
+    {
+        /// <summary>
+        /// get result from trained model
+        /// </summary>
+        /// <returns></returns>
+        double GetResult();
+
+        /// <summary>
+        /// train a model
+        /// </summary>
+        /// <param name="data"></param>
+        void Train(double[] data)
+        {
+
+        }
+
+        /// <summary>
+        /// save a result
+        /// </summary>
+        public void Save(string saveFilename)
+        {
+            GetResult();
+
+            string save_path = String.Concat(Directory.GetCurrentDirectory(),"/",saveFilename);
+
+            System.IO.File.WriteAllText(save_path, GetResult().ToString());
+
+            Console.Write($"Save result in {saveFilename} complete.");
+        }
+
+        /// <summary>
+        /// load a result
+        /// </summary>
+        public double Load(string loadFilename)
+        {
+            string load_path = String.Concat(Directory.GetCurrentDirectory(),"/", loadFilename);
+
+            var readTxt = System.IO.File.ReadAllText(load_path);
+            
+            Console.Write($"Load result in {loadFilename} complete.");
+
+            return Convert.ToDouble(readTxt);
+        }
+    }
+}
---
+++ MyWork/HelloWorldWithInterface/HelloWorldWithInterface/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+
+namespace HelloWorldWithInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            double[] data = new double[] { 100.0, 212.5, 12, 50.4, 21.5};
+
+            IMyInterface sumAlg = new SumAlgorithm();
+
+            IMyInterface avgAlg = new AverageAlgorithm();
+
+            string sum_saveFilename = "sumTrainResult.csv";
+
+            string avg_saveFilename = "avgTrainResult.csv";
+
+            double prevResult = 0;
+
+            // Try to load sum tained result from last time
+            if (File.Exists(sum_saveFilename))
+            {
+                prevResult = sumAlg.Load(sum_saveFilename);
+                Console.WriteLine($"Previous sum result is {prevResult}");
+            }
+
+            // Try to load average tained result from last time
+            if (File.Exists(avg_saveFilename))
+            {
+                prevResult = avgAlg.Load(avg_saveFilename);
+                Console.WriteLine($"Previous avg result is {prevResult}");
+            }
+
+
+            // Train some model
+            double sumResult = TrainModel(sumAlg, data, sum_saveFilename);
+            Console.WriteLine($"Train model with Sum algorithm has output : {sumResult}");
+
+            double avgResult = TrainModel(avgAlg, data, avg_saveFilename);
+            Console.WriteLine($"Train model with Average algorithm has output : {avgResult}");
+
+            Console.WriteLine("Press any key to end session.");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Train a model and return a value base on method
+        /// </summary>
+        /// <param name="alg"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        private static double TrainModel(IMyInterface alg, double[] data, string saveFilename)
+        {
+            alg.Train(data);
+
+            double output = alg.GetResult();
+
+            alg.Save(saveFilename);
+
+            return output;
+        }
+    }
+}
