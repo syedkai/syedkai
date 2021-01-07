87d6e30d2517ca0cdd8a27f49b6951ada24be89a
md fathir ahmed shishir <mfa.shishir@gmail.com>
2021-01-08 02:00:36 +0600

    Hello world with class project added

---
+++ MyWork/HelloWorldWithClass/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorldWithClass
+{
+    class Class1
+    {
+        public string a = "Hello world";
+    }
+}
---
+++ MyWork/HelloWorldWithClass/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using HelloWorldWithClass;
+
+namespace HelloWorldWithClass
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+
+            Console.WriteLine(class1.a);
+        }
+    }
+}
