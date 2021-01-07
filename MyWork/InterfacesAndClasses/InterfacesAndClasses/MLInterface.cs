54bf11a98ff3212cce9d96351d353c011c55a0dd
Shivakumar <shivakumar.haralahallimariyappa@stud.fra-uas.de>
2021-01-07 20:27:21 +0530

    New Ex

---
+++ MyWork/InterfacesAndClasses/InterfacesAndClasses/MLInterface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfacesAndClasses
+{
+    interface MLInterface
+    {
+        public Result GetResult();
+
+        public void Train(double[] data);
+    }
+}
