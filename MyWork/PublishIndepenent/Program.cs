f682515bd2d605492b80226953b3f78784c0e041
md fathir ahmed shishir <mfa.shishir@gmail.com>
2021-01-08 02:08:21 +0600

    Publish independent project

---
+++ MyWork/PublishIndepenent/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PublishIndepenent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello,It is a public independent project!");
+            Console.ReadLine();
+        }
+    }
+}
