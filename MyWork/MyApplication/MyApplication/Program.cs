205af6b62ad92eb5139f82a0204f8ebb25551d12
Aishin Abdulla Yoosufali <aishinahmed20@gmail.com>
2021-01-08 00:40:21 +0100

    Self-Contained Application builded.

---
+++ MyWork/MyApplication/MyApplication/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+namespace MyApplication
+{
+    /// <summary>
+    /// More simplifies version of Task Delay
+    /// </summary>
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello folks! Deep dive in .NET 5.0");
+            int n = 100;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"counter {n}.");
+
+                await Task.Delay(500);
+               
+            }
+            Console.WriteLine("Press any key to exit..");
+            Console.ReadKey();
+
+        }
+
+
+        /// <summary>
+        /// More complex verion with Task Delay
+        /// </summary>
+
+
+        static void Main2(string[] args)
+        {
+            Console.WriteLine("Hello folks! Deep dive in .NET 5.0");
+            int n = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"counter {n}.");
+                Task.Delay(10000).ContinueWith((t) =>
+                {
+                    Console.WriteLine("Done with delay!");
+                });
+                ;
+            }
+            Console.WriteLine("Press any key to exit..");
+            Console.ReadKey();
+
+        }
+    }
+}
