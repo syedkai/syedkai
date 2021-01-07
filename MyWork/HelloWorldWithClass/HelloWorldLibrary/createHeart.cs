5f11085cd642d10a2260dcfe97dc5c4118a3ccc2
Md Rabiul Islam <31253296+rabiul64@users.noreply.github.com>
2021-01-07 21:43:45 +0600

    add Exercise 4 to branch ( HelloWorldWithClass)

---
+++ MyWork/HelloWorldWithClass/HelloWorldLibrary/createHeart.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace HelloWorldLibrary
+{
+    public class createHeart
+    {
+        public void createHeartFromNumber(int n)
+        {
+            int i, j;
+
+            for (i = n / 2; i <= n; i += 2)
+            {
+                for (j = 1; j < n - i; j += 2)
+                {
+                    Console.Write(" ");
+                }
+
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+
+                for (j = 1; j <= n - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.Write("\n");
+            }
+
+            for (i = n; i >= 1; i--)
+            {
+                for (j = i; j < n; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (j = 1; j <= (i * 2) - 1; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.Write("\n");
+            }
+        }
+    }
+}
