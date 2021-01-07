1e40102fec77c0e13db6f738683dd71c8cbb1b4c
md fathir ahmed shishir <mfa.shishir@gmail.com>
2021-01-08 00:50:51 +0600

    Adding debugging project

---
+++ MyWork/WorkingWithDebugger/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace WorkingWithDebugger
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("working with debugger");
+
+            int n = 10;
+
+            for (int i = 0; i < n; i++)
+            {
+
+                Console.WriteLine($"Value {i}.");
+                await Task.Delay(100);
+            }
+
+            Console.WriteLine(" Press enter to exit");
+            Console.ReadLine();
+
+        }
+    }
+}
