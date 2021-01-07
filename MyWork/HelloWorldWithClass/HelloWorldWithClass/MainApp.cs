5129868cb86274173312015c798a82bdb469fb74
Md Rabiul Islam <31253296+rabiul64@users.noreply.github.com>
2021-01-07 21:46:31 +0600

    add Exercise 4 to branch ( HelloWorldWithClass)

---
+++ MyWork/HelloWorldWithClass/HelloWorldWithClass/MainApp.cs
@@ -0,0 +1,28 @@
+﻿using HelloWorldLibrary;
+using System;
+
+
+namespace HelloWorldWithClass
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int input = 0;
+
+            do
+            {
+                Console.Write("Hello ! Please type in some number ( equal or greater than 5) : ");
+
+                input = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"You have entered {input} into the system");
+            }
+            while (input < 5);
+            
+            createHeart heart = new createHeart();
+
+            heart.createHeartFromNumber(input);
+        }
+    }
+}
