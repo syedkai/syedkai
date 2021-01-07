21ccc2663dc6451bff0c8f880784c6ed11bfe176
Sourav <sourav.sumit@stud.fra-uas.de>
2021-01-08 00:09:19 +0600

    Adding project with debugger

---
+++ MyWork/WorkingWithDebugger/WorkingWithDebugger/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+namespace WorkingWithDebugger
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Below You Will See Some Variable Values");
+            int S = 20;
+            for (int P = 5; P < S; P++)
+            {
+                Console.WriteLine($"Reading Value = {P}.");
+                await Task.Delay(200);
+            }
+            Console.WriteLine(" Please press Enter key to exit ");
+            Console.ReadLine();
+        }
+    }
+}
