bc7095f1c1fc640062641778ce644cba9b60abf6
Md Mehadi Hasan <hmd.mehadi@outlook.com>
2021-01-07 02:35:10 +0100

    test

--- MyWork/ProjectWithVS/HelloWorldWithVS_1/HelloWorldWithVS_1/Program.cs
+++ MyWork/ProjectWithVS/HelloWorldWithVS_1/HelloWorldWithVS_1/Program.cs
@@ -7,6 +7,9 @@ namespace HelloWorldWithVS_1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!"); 
+            Console.WriteLine("Hello World!");
         }
     }
 }
