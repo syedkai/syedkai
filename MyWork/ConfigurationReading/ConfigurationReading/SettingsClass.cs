5581061ec24696921ce67e0a53575080756d881f
Shivakumar <shivakumar.haralahallimariyappa@stud.fra-uas.de>
2021-01-07 20:30:06 +0530

    New Ex2

---
+++ MyWork/ConfigurationReading/ConfigurationReading/SettingsClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConfigurationReading
+{
+    class SettingsClass
+    {
+        public string ClientId { get; set; }
+
+        public string ClientConfigName { get; set; }
+
+    }
+}
