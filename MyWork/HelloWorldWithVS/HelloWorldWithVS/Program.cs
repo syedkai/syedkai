f11588c8f4893f5c86ddbf370bb7057126d95a6d
Aishin Abdulla Yoosufali <aishinahmed20@gmail.com>
2021-01-08 00:52:38 +0100

    Exercises

---
+++ MyWork/HelloWorldWithVS/HelloWorldWithVS/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace HelloWorld
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello in .Net World");
+			Console.WriteLine("\nWhat's your name? ");
+			var name = Console.ReadLine();
+			var date = DateTime.Now;
+			Console.WriteLine($"\nHello, {name}, on {date:d}, at {date:t}!");i
+			Console.ReadKey(true);
+		}
+	}
+}
