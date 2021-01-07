2ada65ecff9520ec25f53781cc167412640f6e4f
Md Rabiul Islam <31253296+rabiul64@users.noreply.github.com>
2021-01-08 00:41:59 +0600

    Exercise 6

---
+++ MyWork/DebuggingProject/DebuggingProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DebuggingProject
+{
+    /// <summary>
+    /// This code is written to see how a debugger works
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            while (counter < 100)
+            {
+                Console.WriteLine($"Hello Sabeen! The counter is {counter}");
+                counter++;
+            }
+        }
+    }
+}
