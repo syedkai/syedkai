cade4f434183c9e5e0769a6949661b14578f273e
Md Mehadi Hasan <hmd.mehadi@outlook.com>
2021-01-08 12:52:37 +0100

    Ex-5 (Stand alone build)

---
+++ MyWork/publish-independent/publish-independent/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace publish_independent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a stand alone build/deployment.");
+            Console.ReadLine(); 
+        }
+    }
+}
