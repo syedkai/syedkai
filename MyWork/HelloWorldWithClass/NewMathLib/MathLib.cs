bbca465d686967f2731f4f0c575a5947255b4047
ManojMarigowda <manojkm0811@gmail.com>
2021-01-08 20:04:16 +0100

    Created new class

---
+++ MyWork/HelloWorldWithClass/NewMathLib/MathLib.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewMathLib
+{
+    public static class MathLib
+    {
+       public static bool StartsWithUpper(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+
+            char ch = str[0];
+            return char.IsUpper(ch);
+        }
+
+    }
+}
+
+
