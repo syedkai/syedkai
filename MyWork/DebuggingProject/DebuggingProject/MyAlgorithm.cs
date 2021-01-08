3cac90ff44c2d146a1ef582186a719ca3d084669
Md Mehadi Hasan <hmd.mehadi@outlook.com>
2021-01-08 17:56:20 +0100

    Ex-6 Working with Debugger

---
+++ MyWork/DebuggingProject/DebuggingProject/MyAlgorithm.cs
@@ -0,0 +1,14 @@
+﻿
+namespace DebuggingProject
+{
+    public class MyAlgorithm
+    {
+        public static double Add(double num1, double num2)
+        {
+            return num1 + num2;
+
+        }
+
+        
+    }
+}
---
+++ MyWork/DebuggingProject/DebuggingProject/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace DebuggingProject
+{
+    public class Person
+    {
+        public string fName;
+        public string lName;
+
+        public void fullName()
+        {
+             Console.WriteLine(  "My name is : " + fName +" " +lName);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+            var obj = new Person();
+            obj.fName = "Mehadi";
+            obj.lName = "Hasan";
+            obj.fullName();
+
+
+            var result= MyAlgorithm.Add(2, 4.2);
+            Console.WriteLine(result);
+            
+
+
+
+             
+            
+
+            
+        }
+    }
+}
