8c73ff8633d2fd83bf7daafae4f5588c660690c2
Sourav Paul Sumit <74201147+SouravPaulSumit@users.noreply.github.com>
2021-01-09 01:05:37 +0600

    file for exercise 10

---
+++ MyWork/WorkingWithNuget/TestLibrary/MyAlgorithm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TestLibrary
+{
+    public class MyAlgorithm
+    {
+        /// <summary>
+        /// calculate sum from data array
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public double CalculateSum(double[] data)
+        {
+            double sum = 0;
+
+            foreach(var item in data)
+            {
+                sum += item;
+            }
+
+            return sum;
+        }
+
+        /// <summary>
+        /// calculate average value from data array
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public double CalculateAverage(double[] data)
+        {
+            return this.CalculateSum(data) / data.Length;
+        }
+    }
+}
