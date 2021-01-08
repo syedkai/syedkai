ea34355becbd77028666e4d579959a52d5654405
Aishin Abdulla Yoosufali <aishinahmed20@gmail.com>
2021-01-08 02:31:47 +0100

    Exercise6

---
+++ MyWork/WorkingWithDebugger/WorkingWithDebugger/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WorkingWithDebugger
+{
+    class Program
+    {
+        private static void doWork()
+        {
+            LinkedList<int> c1 = new LinkedList<int>();
+
+            c1.AddLast(10);
+            c1.AddLast(20);
+
+            LinkedList<int> c2 = new LinkedList<int>(c1);
+            int i = c2.First.Value;
+            int j = c2.Last.Value;
+            Console.Write("The first element is ");
+            Console.Write(i);
+            Console.Write("\n");
+            Console.Write("The second element is ");
+            Console.Write(j);
+            Console.Write("\n");
+
+        }
+
+        static int Main()
+        {
+            // using namespace std;
+            doWork();
+            return 0;
+
+        }
+    }
+}
