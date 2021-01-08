061f90b85c72a64a1f58a1d2ae72af82d9733fd4
S M MEHEDI <mehedi.it12@gmail.com>
2021-01-08 12:46:18 +0600

    Excerise 2 and 3

---
+++ MyWork/E2/HelloWorldWithCli/HelloWorldWithCli/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorldWithCli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MyWork/E3/HelloWorldWithVS/HelloWorldWithVS/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorldWithVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Name: S M Mehedi Hasan");
+            Console.WriteLine("University : Frankfurt University of Applied Science");
+            Console.WriteLine("Program : Master of Engineering in IT ");
+            Console.WriteLine("Winter semester 2020/21");
+            Console.ReadLine();
+        }
+    }
+}
