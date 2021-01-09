10b3cf94d3b55fb95bf7f2d6fb268b02e99574c2
ManojMarigowda <manojkm0811@gmail.com>
2021-01-09 17:04:23 +0100

    New Class program

---
+++ MyWork/SquareRootWithClass/SquareRootWithClass/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SquareRootWithClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Z;
+            do
+            {
+
+                Console.WriteLine("Please, enter a number to be squared or enter 0 to exit:");
+                int x = int.Parse(Console.ReadLine());
+                Console.WriteLine(SqrtPrecalculated.GetSqrt(x));
+                Z = x;
+
+            }
+            while (Z != 0);
+            Console.WriteLine("End");
+            System.Threading.Thread.Sleep(1000);
+        }
+    }
+
+}
