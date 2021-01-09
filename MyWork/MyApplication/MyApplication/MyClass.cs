1e6157b825530e345bd3b15c5eb0112876dda76f
Aishin Abdulla Yoosufali <aishinahmed20@gmail.com>
2021-01-09 16:42:11 +0100

    Added MyClass sample to MyApplication

---
+++ MyWork/MyApplication/MyApplication/MyClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApplication2
+{
+    /*private / internal public*/ 
+    public class MyClass
+    {
+        public static double GetLength(string input)       /*String Method of class*/
+        {
+            return input.Length;                  /*length of a string*/
+        }
+        public double Divide(int a, int b)  /*Instance Method*/
+        {
+            return a / b;
+        }
+    }
+}
--- MyWork/MyApplication/MyApplication/Program.cs
+++ MyWork/MyApplication/MyApplication/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyApplication2;
+using System;
 using System.Threading.Tasks;
 namespace MyApplication
 {
@@ -10,6 +11,35 @@ namespace MyApplication
         static async Task Main(string[] args)
         {
             Console.WriteLine("Hello folks! Deep dive in .NET 5.0");
+
+            //Run  sample
+            //await LoopSample();
+
+            RunClassSample();
+
+            Console.WriteLine("Press any key to exit..");
+            Console.ReadKey();
+            
+        }
+
+        private static void RunClassSample()
+        {
+            Console.WriteLine("Please enter some text and I'll calcute the length of the text.");
+
+            var txt = Console.ReadLine();
+
+            var len = MyClass.GetLength(txt);
+
+            Console.WriteLine($"The Length of the Text is: {len}");
+        }
+
+
+        /// <summary>
+        /// Looping samplem with task.delay
+        /// </summary>
+        /// <returns>N.a.</returns>
+        private static async Task LoopSample()
+        {
             int n = 100;
 
             for (int i = 0; i < n; i++)
@@ -17,11 +47,8 @@ namespace MyApplication
                 Console.WriteLine($"counter {i}.");
 
                 await Task.Delay(500);
-               
-            }
-            Console.WriteLine("Press any key to exit..");
-            Console.ReadKey();
 
+            }
         }
 
 
