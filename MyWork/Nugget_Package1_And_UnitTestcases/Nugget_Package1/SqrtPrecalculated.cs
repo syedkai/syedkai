108d0ed31b5bee61d66e13df731ff3b297ad5484
ManojMarigowda <manojkm0811@gmail.com>
2021-01-09 18:28:32 +0100

    Working with Nugget and UnitTestCases

---
+++ MyWork/Nugget_Package1_And_UnitTestcases/Nugget_Package1/SqrtPrecalculated.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nuget_Package1
+{
+    public static class SqrtPrecalculated
+    {
+        public const int MaxValue = 100000;
+
+        private static int[] sqrtValues;
+
+        static SqrtPrecalculated()
+        {
+            sqrtValues = new int[MaxValue + 1];
+            for (int i = 0; i < sqrtValues.Length; i++)
+            {
+                sqrtValues[i] = (int)Math.Sqrt(i);
+            }
+        }
+
+        public static int GetSqrt(int value)
+        {
+            if ((value < 0) || (value > MaxValue))
+            {
+                throw new ArgumentOutOfRangeException(string.Format("The argument should be in range [0..{0}].", MaxValue));
+            }
+            return sqrtValues[value];
+        }
+    }
+}
---
+++ MyWork/Nugget_Package1_And_UnitTestcases/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Nuget_Package1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+           
+            Assert.AreEqual(SqrtPrecalculated.GetSqrt(81), 9);
+        }
+    }
+}
