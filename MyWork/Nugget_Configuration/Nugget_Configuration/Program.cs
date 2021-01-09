8b7cba8e63bab962374c9d200f72c8bcf294a6e4
ManojMarigowda <manojkm0811@gmail.com>
2021-01-09 17:39:15 +0100

    Nugget Configuration

---
+++ MyWork/Nugget_Configuration/Nugget_Configuration/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Nugget_Configuration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome");
+            var configBuilder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            IConfigurationRoot configuration = configBuilder.Build();
+              
+            //reading nested values
+            var client = configuration.GetSection("ClientDetails");
+            Console.WriteLine("The client details are: Name - {0} , Age {1}", client["Name"], client["Age"]);
+            
+        }
+    }
+}
