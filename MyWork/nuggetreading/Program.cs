228a02923c4cbca6c3198c50d0eb05c35ced05d9
Susmita Sumaiya <ssmtsumaiya@gmail.com>
2021-01-10 16:59:42 +0600

    nuggetreading

---
+++ MyWork/nuggetreading/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.Configuration;
+
+namespace nuggetreading
+{
+    class Program
+    {
+        private static ConfigurationBuilder conbuilder;
+        private static IConfigurationRoot conroot;
+        static void Main(string[] args)
+        {
+             conbuilder = new ConfigurationBuilder();
+
+            conbuilder.AddJsonFile("appsettings.json");
+            conbuilder.AddCommandLine(args);
+            conroot = conbuilder.Build();
+            Console.WriteLine(conroot["Setting1"]);
+        }
+    }
+}
