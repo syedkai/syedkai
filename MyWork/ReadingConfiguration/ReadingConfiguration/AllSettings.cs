4656783f6a9387616fcd55047252e1a463ef148e
Md Mehadi Hasan <hmd.mehadi@outlook.com>
2021-01-10 01:29:50 +0100

    Ex-7 (Reading configuration, file)

---
+++ MyWork/ReadingConfiguration/ReadingConfiguration/AllSettings.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace ReadingConfiguration
+{
+    public class AllSettings
+    {
+
+        public string Message { get; set; }
+        public string Department { get; set; }
+        public int Year { get; set; }
+        public MySetting MySetting { get; set; }
+    }
+}
---
+++ MyWork/ReadingConfiguration/ReadingConfiguration/Program.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace ReadingConfiguration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Reading Configuration sample!");
+            Console.WriteLine();
+
+            var builder = new ConfigurationBuilder()
+
+
+            .AddCommandLine(args)
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables();
+
+
+            IConfigurationRoot configuration = builder.Build();
+
+
+            //From command Line
+
+            var name = configuration["name"];
+            var color = configuration["color"];
+            var windows = configuration["windows"];
+            Console.WriteLine("From command line - " + " Name:" + name + "  Color:" + color + "  Windows version:" + windows);
+            Console.WriteLine();
+
+
+            //From root of apppsettings.json
+
+            var message = configuration["Message"];
+            var department = configuration["Department"];
+            var year = configuration["Year"];
+            Console.WriteLine("From appsettings.jason - " + " Message:" + message + "  Dept.:" + department + "  Year:" + year);
+            Console.WriteLine();
+
+
+            //Variable from subsection
+            var subSection = configuration.GetSection("SubSection");
+            var subSectionMessage = subSection["Message"];
+            var subSectionDepartment = subSection["Department"];
+            var subSectionYear = subSection["Year"];
+            Console.WriteLine("Sub-section fromrom appsettings.jason - " + " Message:" + subSectionMessage + "  Dept.:" + subSectionDepartment + "  Year:" + subSectionYear);
+            Console.WriteLine();
+
+            //Reads environment variables
+            var Processor = configuration["PROCESSOR_ARCHITECTURE"];
+            Console.WriteLine("Reads from environt variable, Processor name : " + Processor);
+
+
+            // code for read typed setting
+
+            MySetting mySettings = new MySetting();
+            configuration.GetSection("MySetting").Bind(mySettings);
+            Console.WriteLine("Reads from Typed setting : " + mySettings.String);
+            
+                        // code for load all setting
+                        AllSettings allSettings = new AllSettings();
+                        configuration.Bind(allSettings);
+            Console.WriteLine("Reads from all settings: " + allSettings.Department);
+
+
+            Console.WriteLine();
+
+
+        }
+    }
+}
