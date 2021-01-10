f594a3c51126eb8d560ce1f045d3f1d04cd1568e
Susmita Sumaiya <ssmtsumaiya@gmail.com>
2021-01-10 21:18:24 +0600

    Structure of Application

---
+++ MyWork/StructureofApplication/Person/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StructureofApplication.Person
+{
+    public class Student
+    {
+
+        public string Name;
+        public int MattNo;
+
+        public void Information()
+        {
+            Console.WriteLine("Hello! I am " + Name + " My mattriculation no is " + MattNo);
+        }
+    }
+}
---
+++ MyWork/StructureofApplication/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StructureofApplication.Person;
+
+namespace StructureofApplication
+{
+    public class Student
+    {
+
+        public string Name;
+        public int MattNo;
+
+        public void Information()
+        {
+            Console.WriteLine("Hello! I am " + Name + " My mattriculation no is " + MattNo);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.Name = "Susmita";
+            student.MattNo = 1234;
+
+            student.Information();
+        }
+    }
+}
