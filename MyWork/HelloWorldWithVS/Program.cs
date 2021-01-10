8b0f7ebe32c266764a7191eadc2b06b922d14f8f
Susmita Sumaiya <ssmtsumaiya@gmail.com>
2021-01-10 22:52:50 +0600

    HelloWorldWithVS

---
+++ MyWork/HelloWorldWithVS/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HelloWorldWithVS
+{
+    public class Hello
+    {
+
+        public string Name;
+
+        public void Greetings()
+        {
+            Console.WriteLine("Hello World! I am " + Name);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hello hello = new Hello();
+            hello.Name = "Susmita";
+            
+
+            hello.Greetings();
+        }
+    }
+}
