6a0dbcca3859cab87a76132fb9480c208f45bf4b
Susmita Sumaiya <ssmtsumaiya@gmail.com>
2021-01-10 16:28:18 +0600

    workingwithdebugger

---
+++ MyWork/WorkingwithDebugger/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WorkingwithDebugger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> strList = new List<string>();
+            strList.Add("Test");
+            strList.Add("my");
+            strList.Add("program");
+            foreach(var str in strList)
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
