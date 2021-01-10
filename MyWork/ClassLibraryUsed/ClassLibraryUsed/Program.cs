8206c4a75583a3d9d464be63673dbb04612f38ea
Bilal Shaikh <26bilal@gmail.com>
2021-01-10 19:47:57 +0100

    Ex 4 completed.

---
+++ MyWork/ClassLibraryUsed/ClassLibraryUsed/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using HelloWorldWithClass_library;
+
+namespace ClassLibraryUsed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj = new Class1();
+            double Result = obj.addition(2.5,7.5);
+            Console.WriteLine(Result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MyWork/HelloWorldWithClass_library/HelloWorldWithClass_library/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldWithClass_library
+{
+    public class Class1
+    {
+        public double addition(double x, double y)
+        {
+            return x + y;
+        }
+    }
+}
