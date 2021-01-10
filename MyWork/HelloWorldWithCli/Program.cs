487cc6fffd3f642b4de30665e35a0dbbbe8e31ed
Susmita Sumaiya <ssmtsumaiya@gmail.com>
2021-01-10 19:18:25 +0600

    helloworldwithcli

---
+++ MyWork/HelloWorldWithCli/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorldWithCli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = "Susmita";
+            String platform = "Cli";
+            Console.WriteLine("Hi " + name + "!");
+            Console.WriteLine("Its your first project with {platform}!");
+           
+        }
+    }
+}
