9154fba5897789ca1fec012f9df3f9c6eb82e5ac
Susmita Sumaiya <ssmtsumaiya@gmail.com>
2021-01-10 16:45:00 +0600

    classimplementation

---
+++ MyWork/classimplementation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace classimplementation
+{
+    public class Student
+    {
+
+        public string Name;
+        public int Age;
+        public int MattNo;
+
+        public void Information()
+        {
+            Console.WriteLine("Hello! I am " + Name + " and my age is " + Age + " My mattriculation no is " + MattNo);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.Name = "susmita";
+            student.Age = 25;
+            student.MattNo = 1234;
+
+            student.Information();
+        }
+    }
+}
