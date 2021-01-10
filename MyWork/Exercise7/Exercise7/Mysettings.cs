226ddfcd7cd114436acf21cceb6b872e8e300083
chhavi <chhavi@DESKTOP-H8TL7KQ>
2021-01-10 17:18:43 +0100

    Ex7 Reading Configuration, Reading Files, Reading file

---
+++ MyWork/Exercise7/Exercise7/Mysettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise7
+{
+    class Mysettings
+    {
+        public int SomeInt { get; set; }
+
+        public string SomeString { get; set; }
+
+        public float SomeFloat { get; set; }
+    }
+}
