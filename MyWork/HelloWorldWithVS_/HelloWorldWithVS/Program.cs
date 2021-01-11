18fd28f3b13e2ccc409bd849215b920d2d16ccff
khanparizat <khan.rahman@stud.fra-uas.de>
2021-01-11 01:50:27 -0600

    HelloWorldWithVS

--- MyWork/HelloWorldWithVS_/HelloWorldWithVS/Program.cs
+++ MyWork/HelloWorldWithVS_/HelloWorldWithVS/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using MyNewClass;
 
 namespace HelloWorldWithVS
 {
     class Program
     {
         static void Main(string[] args)
+
         {
+            HelloWorldWithClass hw = new HelloWorldWithClass();
+
             Console.WriteLine("Hello World!");
-        }
+             hw.FirstClass();
+
+    }
     }
 }
