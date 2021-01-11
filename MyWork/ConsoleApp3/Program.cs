1b7135df1334d7644032f500f9cfbdd987f42587
Syed Kaif <Syedkaif339@gmail.com>
2021-01-11 15:18:33 +0500

    Signed-off-by: Syed Kaif <Syedkaif339@gmail.com>

---
+++ MyWork/ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace WorkingWithDebugger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b;
+            int result = 1;
+
+            Console.WriteLine("Enter Number to Calculate Fictorial ");
+            b = int.Parse(Console.ReadLine());
+
+
+            for (int i = b; i > 1; i--)
+            {
+                result = result * i;
+                Console.WriteLine($"Iterration No {a} and result {result}\n");
+                a++;
+
+            }
+        }
+    }
+}
