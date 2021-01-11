c40bc468c0a0520619ac1d505f66a037e87c005c
Farjana <shifafarjana634@gmail.com>
2021-01-12 05:49:13 +0600

    Exercise 4 done

---
+++ MyWork/HelloWorldWithClass/HelloWorldWithClass/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorldWithClass
+{
+
+    class Class1
+    {
+        public int calculation(int a,int b) {
+            return a+b;
+        }
+    }
+}
---
+++ MyWork/HelloWorldWithClass/HelloWorldWithClass/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorldWithClass
+{
+    class Program : Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Class1 add = new Class1();
+            Console.WriteLine( add.calculation(12,14));
+            
+        }
+    }
+}
