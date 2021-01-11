0965b8baa54155dd4de362f79118d226c5ff4737
paweenp <paween.pongsomboon@gmail.com>
2021-01-11 18:43:15 +0100

    add InvestigateHPC Stability Project directory and files to Source folder

---
+++ Source/MyProject/Investigate_HPC/Investigate_HPC/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace investigate_HPC
+{
+    class Program
+    {
+        /// <summary>
+        /// Trial for HPC
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            SpatialPatternLearning spExperiment = new SpatialPatternLearning();
+
+            spExperiment.Run();
+
+        }
+    }
+}
