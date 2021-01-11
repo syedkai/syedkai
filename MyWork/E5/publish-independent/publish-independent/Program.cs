5bcc96611c2fd2f97405d4ad02a93136f1bbb96d
S M MEHEDI <mehedi.it12@gmail.com>
2021-01-11 18:50:50 +0600

    Exercise 5,6,7

---
+++ MyWork/E5/publish-independent/publish-independent/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace publish_independent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" This Independent Framework Build (stand alone build)");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MyWork/E7/NuggetPackages/NuggetPackages/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace NuggetPackages
+{
+    class Program
+    {
+        private static ConfigurationBuilder configurationBuilder ;
+        private static IConfigurationRoot configurationRoot;
+
+        static void Main(string[] args)
+        {
+            configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.AddJsonFile("appsettings.json");
+            configurationBuilder.AddCommandLine(args);
+            configurationRoot = configurationBuilder.Build();
+            Console.WriteLine(configurationRoot["Setting1"]);
+        }
+    }
+}
