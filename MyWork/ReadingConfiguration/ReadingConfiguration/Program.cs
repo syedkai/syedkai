54669d1940329558fc31dad26569c15e52208a67
Sourav <sourav.sumit@stud.fra-uas.de>
2021-01-12 01:18:07 +0600

    file for exercise 7

---
+++ MyWork/ReadingConfiguration/ReadingConfiguration/Program.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace ReadingConfiguration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Reading Configuration Test");
+            Console.WriteLine();
+
+            var builder = new ConfigurationBuilder()
+
+            .AddCommandLine(args)
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables();
+
+
+            IConfigurationRoot configuration = builder.Build();
+
+
+            //From command Line
+            var name = configuration["name"];
+            var color = configuration["color"];
+            var windows = configuration["windows"];
+            var size = configuration["size"];
+            Console.WriteLine("From command line - " + " Name:" + name + "  Color:" + color + "  Windows version:" + windows + " size:" + size);
+            Console.WriteLine();
+
+
+            //From root of apppsettings.json
+            var message = configuration["Message"];
+            var department = configuration["Department"];
+            var University = configuration["University"];
+            var year = configuration["Year"];
+            Console.WriteLine("From appsettings.jason - " + " Message:" + message + "  Dept.:" + department + " University:" + University + " Year:" + year);
+            Console.WriteLine();
+
+
+
+
+
+            //Reads environment variables
+            var Computername = configuration["COMPUTERNAME"];
+            Console.WriteLine("Reads from environt variable -  Computer name : " + Computername);
+
+
+            // code for read typed setting
+            MySetting mySettings = new MySetting();
+            configuration.GetSection("MySetting").Bind(mySettings);
+            Console.WriteLine("Reads from Typed setting - " + mySettings.String + "  " + mySettings.Float +"  "+ mySettings.Int );
+            
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
