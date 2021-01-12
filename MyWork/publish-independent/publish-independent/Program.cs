2bf0a6eae312300a05ab73d9309d2ba98822a88b
Farjana <shifafarjana634@gmail.com>
2021-01-12 21:35:07 +0600

    Exercise 5 is done

---
+++ MyWork/publish-independent/publish-independent/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace publish_independent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is the Publish Independent project");
+
+        }
+    }
+}
