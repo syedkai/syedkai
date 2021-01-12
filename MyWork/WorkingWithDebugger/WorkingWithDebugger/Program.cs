4133a63435c94d68ef41e4c82b3b862f856a4141
Farjana <shifafarjana634@gmail.com>
2021-01-13 00:48:08 +0600

    Exercise 6 is done

---
+++ MyWork/WorkingWithDebugger/WorkingWithDebugger/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WorkingWithDebugger
+{
+    class Program
+    {
+        static void MyMethod(string country = "Norway")
+        {
+            Console.WriteLine(country);
+        }
+
+        static void Main(string[] args)
+        {
+            MyMethod("Sweden");
+            MyMethod("India");
+            MyMethod();
+            MyMethod("USA");
+        }
+    }
+}
