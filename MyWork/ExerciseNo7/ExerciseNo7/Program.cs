0bd05654b586b5d261f4098a885c9273ce377cec
Syed Kaif <Syedkaif339@gmail.com>
2021-01-12 14:06:15 +0500

    Exercise No 7 Done

---
+++ MyWork/ExerciseNo7/ExerciseNo7/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleAppSettings
+{
+    class Program
+    {
+        
+
+
+
+        static void Main(string[] args)
+        {
+            string filepath = @"C:\Users\ICT\OneDrive\Desktop\vs\MyWork\ExerciseNo7\ExerciseNo7\App2.json";
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            IConfiguration builder = new ConfigurationBuilder()
+
+           .SetBasePath(filepath)
+           .AddJsonFile("App2.json", optional: true, reloadOnChange: true)
+           .AddCommandLine(args)
+           .AddEnvironmentVariables()
+           .Build();
+
+            var Set1 = builder["Setting3"];                  
+            Console.WriteLine($"{Set1}");
+
+            var Set2 = builder["Sets"];                     
+            Console.WriteLine($"{Set2}");
+
+            Set2 = builder["ProgramFiles"];                         
+            Console.WriteLine($"{Set2}");
+
+            //MySetting Set = new MySetting();
+            //builder.GetSection("SomeInt").Bind(Set);
+
+            Set2 = builder["MySetting:SomeString"];
+            Console.WriteLine($"{Set2}");
+
+
+            //AllSetting allSettings = new AllSetting();
+            //builder.Bind(allSettings);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
+
