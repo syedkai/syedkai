816a2c98afc4aad7571158785555a1794540268c
Haritha G <harytha6@gmail.com>
2021-01-13 14:44:18 +0100

    final

---
+++ MyWork/Haritha/E9 - Parallel Processing/AlgorithmforMulti-SingleThreading.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace E9___Parallel_Processing
+{
+    class AlgorithmforMultiSingleThreading
+    {
+        private long m_FinishCounter = 0;
+
+
+
+        // one by one execution
+        public void Sequential(int threadcount, Action<object> func)
+        {
+            Thread.CurrentThread.Name = "Sequential - 1 Thread";
+
+            for (int i = 0; i < threadcount; i++)
+            {
+                func(null);
+            }
+
+
+        }
+
+
+
+        // Multi threading with threads <= no of cores
+        public void Multicore(int threads, int Threadcount, Action<object> func)
+        {
+            List<Thread> threadList = new List<Thread>();
+            for (int i = 0; i < Threadcount; i++)
+            {
+                var t = new Thread(new ParameterizedThreadStart(func));
+                t.Name = i.ToString();
+                threadList.Add(t);
+            }
+            if (threads == 2)
+            {                
+                    for (int j = 0; j < 50; j++)
+                    {
+                        threadList[j].Start(new Action<string>(OnThreadFinished));
+                    }
+                    for (int j = 50; j < 100; j++)
+                    {
+                        threadList[j].Start(new Action<string>(OnThreadFinished));
+                    }
+
+            }
+            if (threads == 3)
+            {
+                for (int j = 0; j < 33; j++)
+                {
+                    threadList[j].Start(new Action<string>(OnThreadFinished));
+                }
+                for (int j = 33; j < 67; j++)
+                {
+                    threadList[j].Start(new Action<string>(OnThreadFinished));
+                }
+                for (int j = 67; j < 100; j++)
+                {
+                    threadList[j].Start(new Action<string>(OnThreadFinished));
+                }
+
+            }
+            if (threads == 4)
+            {
+                for (int j = 0; j < 25; j++)
+                {
+                    threadList[j].Start(new Action<string>(OnThreadFinished));
+                }
+                for (int j = 25; j < 50; j++)
+                {
+                    threadList[j].Start(new Action<string>(OnThreadFinished));
+                }
+                for (int j = 50; j < 75; j++)
+                {
+                    threadList[j].Start(new Action<string>(OnThreadFinished));
+                }
+                for (int j = 75; j < 100; j++)
+                {
+                    threadList[j].Start(new Action<string>(OnThreadFinished));
+                }
+            }
+
+            while (true)
+            {
+                if (Interlocked.Read(ref m_FinishCounter) == Threadcount)
+                    break;
+                else
+                    Thread.Sleep(500);
+            }
+        }
+
+
+
+        // Multi Threading with context switches happening
+        public void Multicontextswitch(int threadcount, Action<object> func1)
+        {
+            List<Thread> threadList = new List<Thread>();
+
+            for (int i = 0; i < threadcount; i++)
+            {
+                var t = new Thread(new ParameterizedThreadStart(func1));
+                t.Name = i.ToString();
+                threadList.Add(t);
+            }
+            // starting the threads
+            for (int m = 0; m < threadcount; m++)
+            {
+                threadList[m].Start(new Action<string>(OnThreadFinished));
+                // thread.Start always accepts only one argument, so create an array to pass more than one
+            }
+            while (true)
+            {
+                if (Interlocked.Read(ref m_FinishCounter) == threadcount)
+                    break;
+                else
+                    Thread.Sleep(500);
+            }
+        }
+
+
+
+        // internal scheduler doing tasks parallely
+        public void Parallel(int threadcount, Action<object> func)
+        {
+            List<Task> tList = new List<Task>();
+
+            for (int i = 0; i < threadcount; i++)
+            {
+                var t = new Task(func, new Action<string>(OnThreadFinished));
+                tList.Add(t);
+            }
+
+            foreach (var t in tList)
+            {
+                t.Start();
+            }
+
+            Task.WaitAll(tList.ToArray());
+        }
+
+
+        //Counter mode to exit
+        public void OnThreadFinished(string threadName)
+        {
+            Interlocked.Increment(ref m_FinishCounter);
+        }
+    }
+}
---
+++ MyWork/Haritha/E9 - Parallel Processing/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace E9___Parallel_Processing
+{
+    class Program
+    {
+        static int numThreads = 100;
+
+        // Rotating 1 large jpeg image in Sequential, Parallel and Threading
+        // Adding delay of 500 ms in each iteration.
+        static string[] files = Directory.GetFiles(@"D:\Haritha\Frankfurt Uni\Schedule 1st semester\Software Engineering\se-cloud-2020-2021\MyWork\Haritha\E9 - Parallel Processing\Original", "*.jpg");
+        static string newpath = @"D:\Haritha\Frankfurt Uni\Schedule 1st semester\Software Engineering\se-cloud-2020-2021\MyWork\Haritha\E9 - Parallel Processing\Modified";
+       static AlgorithmforMultiSingleThreading instance = new AlgorithmforMultiSingleThreading();
+
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(newpath);
+            Console.WriteLine("Press any Key to start.");
+            Console.ReadKey();
+            //Console.ReadLine();
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            // Executes all jobs sequentianally
+           // instance.Sequential(numThreads, workerFunction);
+
+            // 2.  Executes with number of threads = no of cores(4)
+
+             // instance.Multicore(2,numThreads, workerFunction); // 
+            //instance.Multicore(3,numThreads, workerFunction);
+            // instance.Multicore(4, numThreads, workerFunction);
+
+            // 3. Executes rotation of image, each in one thread, so CONTEXT SWITCHING 
+            // instance.Multicontextswitch(numThreads, workerFunction);
+
+            // 4. Execute with internal scheduler parallel
+             instance.Parallel(numThreads, workerFunction);
+            
+            sw.Stop();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("{0} ms", sw.ElapsedMilliseconds);
+
+            Console.ReadLine();
+        }
+
+        private static void workerFunction(object onFinishDelegate)
+        {
+            
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Yellow");
+
+            Console.WriteLine($"Started thread: {Thread.CurrentThread.Name} - {Thread.CurrentThread.ManagedThreadId}");
+
+            string fileorig = Path.GetFileName(files[0]);
+            Console.WriteLine(fileorig);
+                string filename = Thread.CurrentThread.ManagedThreadId+".jpg";
+                var bitmap = new Bitmap(files[0]);
+
+                bitmap.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                bitmap.Save(Path.Combine(newpath, filename));
+
+            Task.Delay(1000);
+                        
+            if (onFinishDelegate != null)
+            {
+                ((Action<string>)onFinishDelegate)(Thread.CurrentThread.Name);
+                
+            }
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Blue");
+
+            Console.WriteLine("Stopped thread: {0}", Thread.CurrentThread.Name);
+        }
+
+        
+    }
+
+}
