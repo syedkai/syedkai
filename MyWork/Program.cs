c25a53bf5587eb83cff3daab7129daca2f0e7b13
Farrukhshafique <76734938+Farrukhshafique@users.noreply.github.com>
2021-01-14 21:53:36 +0500

    Working with Debugger

---
+++ MyWork/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int i, fact = 1, number;
+            Console.Write("Enter any Number: ");
+            number = int.Parse(Console.ReadLine());
+            for (i = 1; i <= number; i++)
+            {
+                fact = fact * i;
+            }
+            Console.Write("Factorial of " + number + " is: " + fact);
+        }
+    }
+}
