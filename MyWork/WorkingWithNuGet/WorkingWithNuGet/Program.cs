69069afba4d97c7074b48903af0b7c2f90fe8e8b
md fathir ahmed shishir <mfa.shishir@gmail.com>
2021-01-14 20:48:46 +0600

    Exercise 10

---
+++ MyWork/WorkingWithNuGet/WorkingWithNuGet/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WorkingWithNuGet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Working with nuget");
+
+
+
+            MyLibrary.MyLib ReferencedLib = new MyLibrary.MyLib();
+            var result =ReferencedLib.Add(12, 2);
+
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+
+
+           
+
+        }
+    }
+}
