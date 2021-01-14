ba816af61c67f4f01f189c8284553e89d40ad073
SHIVAM KAUSHIK <shivamkaushik007@yahoo.com>
2021-01-14 22:20:19 +0100

    ReadConfig was created earlier but some modification is done as program is having some problem

---
+++ MyWork/AppsettingJson/AppsettingJson/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppsettingJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
