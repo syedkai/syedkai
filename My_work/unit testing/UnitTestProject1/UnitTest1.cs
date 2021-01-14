cf0d34e7eb7867246a2eec8ec565c70806b2037d
Mandar Hate <mandarhate6897@outlook.com>
2021-01-14 21:43:43 +0100

    Unit testing done

---
+++ My_work/unit testing/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,37 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using unit_testing;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [DataRow(10, 20, 10, 40)]
+        [DataRow(20, 20, 20, 60)]
+        [DataRow(40, 20, 40, 90)]
+        public void TestAdd(int a, int b, int c, int expected)
+        {
+
+            Class1 obj = new Class1();
+            var result = obj.Add(a, b, c);
+            Assert.AreEqual(expected, result);
+
+        }
+
+        [TestMethod]
+
+        public void TestName()
+        {
+        }
+
+        [TestMethod]
+
+        public void TestMul()
+        {
+            Class1 obj = new Class1();
+            var result = obj.Mul(72, 1);
+            Assert.AreEqual(72, result);
+        }
+    }
+}
