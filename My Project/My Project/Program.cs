4101e448f17c8497cb5fcded9c260d3df1157b54
chhavi <chhavi@DESKTOP-H8TL7KQ>
2021-01-14 18:29:10 +0100

    trial

---
+++ My Project/My Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace My_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!!!");
+        }
+    }
+}
