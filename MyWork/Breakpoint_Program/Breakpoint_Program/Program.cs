9596b3f771976d92350f6af03a82dc6de7af013d
SHIVAM KAUSHIK <shivamkaushik007@yahoo.com>
2021-01-14 15:51:22 +0100

    Debugging Demonstrated using Break Point

---
+++ MyWork/Breakpoint_Program/Breakpoint_Program/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Breakpoint_Program
+{
+    class Program
+    {
+
+        public void Test1()
+
+        {
+            Console.WriteLine("First Processing Start");
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("The Value of i is :" +i);
+
+            }
+
+            Console.WriteLine("Second Processing Start");
+
+
+            for (int x = 0; x < 100; x++)
+            {
+                Console.WriteLine("The Value of x is :" + x);
+
+            }
+
+        }
+        static void Main(string[] args)
+        {
+
+            Program obj = new Program();
+            obj.Test1();
+
+        }
+    }
+}
