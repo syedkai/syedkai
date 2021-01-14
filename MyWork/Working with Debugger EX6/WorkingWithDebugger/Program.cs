cd6c3b7b75471e6e80c331e5c3fa81eb42b1cd45
Sumi <nusrat.sumi85@gmail.com>
2021-01-14 23:21:58 +0600

    WorkingWithDebugger EX6

---
+++ MyWork/Working with Debugger EX6/WorkingWithDebugger/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WorkingWithDebugger
+{
+    class Program
+    {
+        static void MyMethod(string name = "Sumi")
+        {
+            Console.WriteLine(name);
+        }
+        static void Main(string[] args)
+        {
+            MyMethod("Rajib");
+            MyMethod("Rita");
+            MyMethod();
+            MyMethod("Rajia");
+        }
+    }
+}
