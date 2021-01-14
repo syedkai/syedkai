20060b34ff303de8a2e202e9f403ff9046b87317
sweta13 <sweta.swain@stud.fra-uas.de>
2021-01-14 16:04:39 +0100

    Commiting Debugging changes

---
+++ MyWork/Ex-6_Debugging/Debug_Async/Debug_Async/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Debug_Async
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Debugging my program");
+            int count = 100;
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine($"The count is {i}");
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
