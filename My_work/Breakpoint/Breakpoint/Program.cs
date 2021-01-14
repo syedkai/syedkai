9602c23e04c3fe013c25d7eb8a049cfa3dd004c8
Mandar Hate <mandarhate6897@outlook.com>
2021-01-14 15:52:15 +0100

    Implemented Breakpoints

---
+++ My_work/Breakpoint/Breakpoint/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Breakpoint
+{
+    class Program
+    {
+        public void Test1()
+
+        {
+            Console.WriteLine("first processing");
+
+            for (int i = 0; i < 50; i++)
+            {
+                Console.WriteLine("the value of i is", +i);
+            }
+
+            Console.WriteLine("second processing");
+        }
+
+            static void Main(string[] args)
+            {
+                Program obj = new Program();
+                obj.Test1();
+            }
+        
+    }
+
+}
