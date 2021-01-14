cfe27affe2854a94275bdc26757d24a3a35abca1
Mandar Hate <mandarhate6897@outlook.com>
2021-01-14 21:59:29 +0100

    Read configuration for appsettings.json

---
+++ My_work/Appsettings.json/Appsettings.json/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.CommandLine;
+using Microsoft.Extensions.Configuration.EnvironmentVariables;
+using Microsoft.Extensions.Configuration.Json;
+
+
+namespace ReadConfig_Appsetting.json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+           .SetBasePath(Directory.GetCurrentDirectory())
+           .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+           .AddCommandLine(args)
+           .AddEnvironmentVariables();
+
+
+            IConfigurationRoot configuration = builder.Build();
+
+
+            var color = configuration["color"];
+            var Setting1 = configuration["Setting1"];
+
+
+
+
+
+            Console.WriteLine("{0}", color);
+            Console.WriteLine("{0}", Setting1);
+
+        }
+    }
+}
