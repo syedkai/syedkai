00b5171469c2ac5fff11849f9049905f4b7bcdf2
md fathir ahmed shishir <mfa.shishir@gmail.com>
2021-01-15 00:29:56 +0600

    Exercise 9

---
+++ MyWork/ParallelProcessing/ParallelClass.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelProcessing
+{
+    class ParallelClass
+    {
+        private long m_FinishCounter = 0;
+
+       
+
+
+        public void StartSequenced(int sequences, Action<object> func)
+        {
+            Thread.CurrentThread.Name = "SingleThread";
+
+            for (int i = 0; i < sequences; i++)
+            {
+                func(null);
+            }
+        }
+
+        /// <param name="threads"></param>
+        /// <param name="func"></param>
+        public void StartMultithreadedNative(int threads, Action<object> func)
+        {
+            for (int i = 0; i < threads; i++)
+            {
+                var t = new Thread(new ParameterizedThreadStart(func));
+                t.Name = i.ToString();
+                t.Start(new Action<string>(OnThreadFinished));
+            }
+
+            while (true)
+            {
+                if (Interlocked.Read(ref m_FinishCounter) == threads)
+                    break;
+                else
+                    Thread.Sleep(500);
+            }
+        }
+
+        private void OnThreadFinished(string threadName)
+        {
+            Interlocked.Increment(ref m_FinishCounter);
+        }
+
+
+        /// <summary>
+        ///3. First creates all threads and the run them.
+        /// </summary>
+        /// <param name="threads"></param>
+        /// <param name="func"></param>
+        public void StartMultithreadedNativeV2(int threads, Action<object> func)
+        {
+            List<Thread> tList = new List<Thread>();
+
+            for (int i = 0; i < threads; i++)
+            {
+                var thread = new Thread(new ParameterizedThreadStart(func));
+                thread.Name = i.ToString();
+                tList.Add(thread);
+            }
+
+            foreach (var item in tList)
+            {
+                item.Start(new Action<string>(OnThreadFinished));
+            }
+
+            while (true)
+            {
+                if (Interlocked.Read(ref m_FinishCounter) == threads)
+                    break;
+                else
+                    Thread.Sleep(500);
+            }
+        }
+
+        
+        public void StartWithTpl(int threads, Action<object> func)
+        {
+            List<Task> tList = new List<Task>();
+
+            for (int i = 0; i < threads; i++)
+            {
+                var t = new Task(func, new Action<string>(OnThreadFinished));
+                tList.Add(t);
+            }
+
+            foreach (var t in tList)
+            {
+                t.Start();
+            }
+
+            Task.WaitAll(tList.ToArray());
+        }
+    }
+
+
+}
---
+++ MyWork/ParallelProcessing/Program.cs
@@ -0,0 +1,60 @@
+﻿
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+
+namespace ParallelProcessing
+{
+    class Program
+    {
+        static int numThreads = 150;
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type any key to start");
+            Console.ReadKey();
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            ParallelClass exe = new ParallelClass();
+
+
+        
+            exe.StartMultithreadedNativeV2(numThreads, workerFunction);
+
+         
+
+            sw.Stop();
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("{0} ms", sw.ElapsedMilliseconds);
+
+        }
+
+
+        private static void workerFunction(object onFinishDelegate)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine($"Started thread: {Thread.CurrentThread.Name} - {Thread.CurrentThread.ManagedThreadId}");
+
+            double r = 202020203030442;
+            for (int i = 0; i < 35000000; i++)
+            {
+                r = r * 1.94536;
+            }
+
+            if (onFinishDelegate != null)
+            {
+                ((Action<string>)onFinishDelegate)(Thread.CurrentThread.Name);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine("Stopped thread: {0}", Thread.CurrentThread.Name);
+        }
+    }
+}
