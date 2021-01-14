3fe7387d47727b3ff02d381dccc9d720fb5cd2b6
Bilal Shaikh <26bilal@gmail.com>
2021-01-14 17:04:59 +0100

    Interface exercise completed.

---
+++ MyWork/Interface_Ex/Interface_Ex/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceTrial2
+{
+    interface IMyInterface1
+    {
+
+        void train(int[] data);
+
+
+    }
+
+    interface IMyInterface2
+
+    {
+
+        void train2(int[] data);
+
+    }
+
+}
