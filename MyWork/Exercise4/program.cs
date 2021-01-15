5d9575b466d2c2495be7aeb646c6f89d0cd751bc
TanzeemHasan <74203937+TanzeemHasan@users.noreply.github.com>
2021-01-15 12:34:17 +0600

    Create program.cs

---
+++ MyWork/Exercise4/program.cs
@@ -0,0 +1,16 @@
+using ClassRefLib;
+using System;
+
+namespace HelloWorldWithClassRef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Display ob = new Display();
+            ob.show();
+
+        }
+    }
+}
