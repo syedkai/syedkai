7e15bfe3ae3ba0ce7d7ba50593e0313c59e2dec2
TanzeemHasan <74203937+TanzeemHasan@users.noreply.github.com>
2021-01-16 01:37:32 +0600

    Independent Framework Build (stand alone build/deployment)

---
+++ MyWork/Example 5/Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace publish_independent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Console.WriteLine("This is a publish independent project. Press any key to end the session.");
+
+                Console.ReadLine();
+            
+        }
+    }
+}
