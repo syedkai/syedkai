2e39656c13a66b8b159521af79289380b4d3f32a
Kizito Onyema <onyemakizito@outlook.com>
2021-01-15 03:28:16 +0100

    AppWithCLI and AppWithVS task

---
+++ MyWork/AppWithVS/AppWithVS/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppWithVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Hello World With VS");
+
+            Console.ReadLine();
+
+        }
+    }
+}
