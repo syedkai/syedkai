e834f5daa27a1b296fb70f56d27fb3ba2626b78d
Sumi <nusrat.sumi85@gmail.com>
2021-01-16 03:25:50 +0600

    Reading Config EX7

---
+++ MyWork/Reading Config  Ex 7/Ex7/AllSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigurationReader
+{
+    class AllSettings
+    {
+        public string message { get; set; }
+
+        public string major { get; set; }
+
+        public float cgpa { get; set; }
+    }
+}
---
+++ MyWork/Reading Config  Ex 7/Ex7/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace ConfigurationReader
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("jsconfig.json", optional: false, reloadOnChange: true)
+            .AddCommandLine(args)
+            .AddEnvironmentVariables();
+
+
+            IConfigurationRoot configuration = builder.Build();
+
+            //from environment variables
+            var user = configuration["USERNAME"];
+
+         
+
+            //from appsettings.json
+            var message = configuration["message"];
+            var major = configuration["major"];
+            var cgpa = configuration["cgpa"];
+
+            //  from command line
+            var subject = configuration["subject"];
+            var department = configuration["department"];
+
+            //loading settings
+            AllSettings allSettings = new AllSettings();
+            configuration.Bind(allSettings);
+
+            Console.WriteLine($"{message} {user}");
+            Console.WriteLine($"Your major is in {major}");
+            Console.WriteLine($"& CGPA is {cgpa} in German Scale");
+            Console.ReadLine();
+
+        }
+    }
+}
