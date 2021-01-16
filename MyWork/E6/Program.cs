395eee11911421b9abb139946ef49e148f3435c3
Nicodemus-Onah <74201192+Nicodemus-Onah@users.noreply.github.com>
2021-01-16 12:02:21 +0100

    Exercise 6

---
+++ MyWork/E6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace E6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 5;
+            int c = a + b;
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+}
