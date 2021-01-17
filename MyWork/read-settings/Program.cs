e7c817e08670e7f80928cfef519aa20e1a3b1e95
Farrukhshafique <76734938+Farrukhshafique@users.noreply.github.com>
2021-01-17 05:04:57 +0500

    Ex.07 Reading Configuration

---
+++ MyWork/read-settings/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.EnvironmentVariables;
+
+namespace read_settings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+           .SetBasePath(Directory.GetCurrentDirectory())
+           .AddJsonFile("json1.json", optional: false, reloadOnChange: true)
+           .AddEnvironmentVariables();
+            if (args != null)
+            {
+                builder.AddCommandLine(args);
+            }
+
+            IConfigurationRoot configuration = builder.Build();
+
+            // Following are comming from command line.           
+            var Username = configuration["Username"];            
+            var Password = configuration["Password"];
+            var Email = configuration["Email"];
+            Console.WriteLine($"{Username}");
+            Console.WriteLine($"{Password}");
+            Console.WriteLine($"{Email}");
+        }
+    }
+}
