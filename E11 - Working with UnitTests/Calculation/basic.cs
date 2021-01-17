c668ceff539c5613985f699e66850d0f3ba2e91f
syedkai <74463966+syedkai@users.noreply.github.com>
2021-01-17 11:14:34 -0800

    ex 11
    exercise 11

---
+++ E11 - Working with UnitTests/Calculation/basic.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Calculation
+{
+    public class basic
+    {
+        public double Ddivide(int a, int b)
+        {
+            return (double)a /(double) b;
+        }
+      
+        public double Mmultiply(int a, int b)
+        {
+            return (double)a * (double)b;
+        }
+        public double Aadd(int a, int b)
+        {
+            return (double)a + (double)b;
+        }
+        public double Ssubtract(int a, int b)
+        {
+            return (double)a - (double)b;
+        }
+    }
+}
