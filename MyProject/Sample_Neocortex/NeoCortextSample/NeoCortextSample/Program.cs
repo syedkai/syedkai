2359ff876c77394cc76d802828e6858606a90a8c
SnigdhaKantam <snigdhakatm@gmail.com>
2021-01-17 22:01:01 +0530

    Adding sample files

---
+++ MyProject/Sample_Neocortex/NeoCortextSample/NeoCortextSample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NeoCortexSample;
+
+namespace NeoCortextSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SequenceLearning experiment = new SequenceLearning();
+            experiment.Run();
+        }
+    }
+}
---
+++ MyProject/Sample_Neocortex/NeoCortextSample/UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("de68e8df-1223-4fb3-a8a6-08a7c67ef51d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
