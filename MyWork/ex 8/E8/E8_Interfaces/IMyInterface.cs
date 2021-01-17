abc5f0abc3c32893af2b8276e26623e6be711fee
syedkai <74463966+syedkai@users.noreply.github.com>
2021-01-17 10:59:34 -0800

    ex 8 done

---
+++ MyWork/ex 8/E8/E8_Interfaces/IMyInterface.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E8_Interfaces
+{
+    interface IMyInterface
+    {
+        object GetResult();
+        void Train(double[] data);
+    }
+}
---
+++ MyWork/ex 8/E8/E8_Interfaces/SumImpl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E8_Interfaces
+{
+    class SumImpl : IMyInterface
+    {
+        private double sum;
+        public object GetResult()
+        {
+            return this.sum;
+        }
+
+        public void Train(double[] data)
+        {
+            foreach(double Value in data){
+                this.sum += Value;
+            }
+            if (this.sum >= 100)
+            {
+                this.sum = 1;
+            }
+            else
+                this.sum = 0;
+        }
+    }
+}
